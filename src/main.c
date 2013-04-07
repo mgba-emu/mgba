68f2eed84d0290500f05a4b3973bb45adf330de7
Jeffrey Pfau <jeffrey@endrift.com>
2013-04-07 01:39:49 -0700

    Mini-test

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
+#include "arm.h"
 #include "gba.h"
+
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(int argc, char** argv) {
 	struct GBA gba;
 	GBAInit(&gba);
+	int fd = open("test.rom", O_RDONLY);
+	GBALoadROM(&gba, fd);
+	gba.cpu.gprs[ARM_PC] = 0x08000004;
+	ARMStep(&gba.cpu);
 	GBADeinit(&gba);
 
 	return 0;
