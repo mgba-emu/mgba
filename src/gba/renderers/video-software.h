265bbe4f8fed33baff0f6340ac0f36965b2f24ed
Jeffrey Pfau <jeffrey@endrift.com>
2013-04-21 13:17:15 -0700

    Add struct for keeping track of background state

--- src/gba/renderers/video-software.h
+++ src/gba/renderers/video-software.h
@@ -5,6 +5,28 @@
 
 #include <pthread.h>
 
+struct GBAVideoSoftwareBackground {
+	int index;
+	int enabled;
+	int priority;
+	uint32_t charBase;
+	int mosaic;
+	int multipalette;
+	uint32_t screenBase;
+	int overflow;
+	int size;
+	uint16_t x;
+	uint16_t y;
+	uint32_t refx;
+	uint32_t refy;
+	uint16_t dx;
+	uint16_t dmx;
+	uint16_t dy;
+	uint16_t dmy;
+	uint32_t sx;
+	uint32_t sy;
+};
+
 struct GBAVideoSoftwareRenderer {
 	struct GBAVideoRenderer d;
 
@@ -12,6 +34,8 @@ struct GBAVideoSoftwareRenderer {
 	unsigned outputBufferStride;
 
 	union GBARegisterDISPCNT dispcnt;
+
+	struct GBAVideoSoftwareBackground bg[4];
 
 	pthread_mutex_t mutex;
 	pthread_cond_t cond;
