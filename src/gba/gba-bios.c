738b3dc44e1d8f7eb3179b0b07c85bae7dc4123b
Jeffrey Pfau <jeffrey@endrift.com>
2013-05-04 15:50:42 -0700

    Implement ArcTan2

--- src/gba/gba-bios.c
+++ src/gba/gba-bios.c
@@ -201,6 +201,9 @@ void GBASwi16(struct ARMBoard* board, int immediate) {
 	case 0x8:
 		gba->cpu.gprs[0] = sqrt(gba->cpu.gprs[0]);
 		break;
+	case 0xA:
+		gba->cpu.gprs[0] = (atan2f(gba->cpu.gprs[0] / 16384.f, gba->cpu.gprs[1] / 16384.f) / 2 * M_PI) * 0x10000;
+		break;
 	case 0xB:
 		_CpuSet(gba);
 		break;
