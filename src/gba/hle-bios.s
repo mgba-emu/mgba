ee344f0dcab22151b49a1f2eff693bb904741366
Jeffrey Pfau <jeffrey@endrift.com>
2013-10-15 00:50:07 -0700

    Optimize and fix IntrWait, make sure that SVC doesn't trample its input register r0

--- src/gba/hle-bios.s
+++ src/gba/hle-bios.s
@@ -18,13 +18,15 @@ swiBase:
 cmp    sp, #0
 moveq  sp, #0x04000000
 subeq  sp, #0x20
-stmfd  sp!, {lr}
-ldrb   r0, [lr, #-2]
-cmp    r0, #4
+stmfd  sp!, {r2, lr}
+ldrb   r2, [lr, #-2]
+cmp    r2, #4
 bleq   IntrWait
-cmp    r0, #5
+cmp    r2, #5
+moveq  r0, #1
+moveq  r1, #1
 bleq   IntrWait
-ldmfd  sp!, {lr}
+ldmfd  sp!, {r2, lr}
 movs   pc, lr
 
 irqBase:
@@ -37,32 +39,31 @@ subs   pc, lr, #4
 
 IntrWait:
 stmfd  sp!, {r4, lr}
-add    sp, #-4
-strh   r1, [sp, #0]
+# Save inputs
+mrs    r3, cpsr
+bic    r3, #0x80
+msr    cpsr, r3
+# Pull current interrupts enabled and add the ones we need
 mov    r4, #0x04000000
-add    r4, #0x200
-ldrh   r0, [r4, #0]
-strh   r0, [sp, #2]
-ldrh   r1, [sp, #0]
-orr    r0, r1
-strh   r0, [r4, #0x0]
-mov    r4, #0x04000000
-IntrWaitLoop:
-mov    r0, #0x1F
-msr    cpsr, r0
+# See if we want to return immediately
+cmp    r0, #0
 mov    r0, #0
+mov    r2, #1
+beq    .L1
+# Halt
+.L0:
 strb   r0, [r4, #0x301]
-mov    r0, #0xD3
+.L1:
+# Check which interrupts were acknowledged
+strb   r2, [r4, #0x204]
+ldrh   r3, [r4, #-8]
+ands   r3, r1
+eorne  r3, r1
+strneh r3, [r4, #-8]
+strb   r0, [r4, #0x204]
+beq .L0
+#Restore state
+mrs    r0, cpsr
+orr    r0, #0x80
 msr    cpsr, r0
-ldrh   r0, [r4, #-8]
-ldrh   r1, [sp, #0]
-ands   r1, r0
-eorne  r1, r0
-strneh r1, [r4, #-8]
-beq    IntrWaitLoop
-mov    r4, #0x04000000
-add    r4, #0x200
-ldrh   r0, [sp, #2]
-strh   r0, [r4, #0]
-add    sp, #4
 ldmfd  sp!, {r4, pc}
