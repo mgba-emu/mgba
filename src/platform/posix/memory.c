b8167f55b1e69819ee4c74174502b9f31fbb5538
Jeffrey Pfau <jeffrey@endrift.com>
2014-01-15 01:28:30 -0800

    Create Windows and POSIX platform directories

---
+++ src/platform/posix/memory.c
@@ -0,0 +1,18 @@
+#include "memory.h"
+
+#include <sys/mman.h>
+
+void* anonymousMemoryMap(size_t size) {
+	return mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+}
+void* fileMemoryMap(int fd, size_t size, int flags) {
+	int mmapFlags = MAP_PRIVATE;
+	if (flags & MEMORY_WRITE) {
+		mmapFlags = MAP_SHARED;
+	}
+	return mmap(0, size, PROT_READ | PROT_WRITE, mmapFlags, fd, 0);
+}
+
+void mappedMemoryFree(void* memory, size_t size) {
+	munmap(memory, size);
+}
--- src/platform/windows/memory.c
+++ src/platform/windows/memory.c
@@ -1,6 +1,5 @@
 #include "memory.h"
 
-#ifdef _WIN32
 #include <io.h>
 #include <Windows.h>
 
@@ -29,21 +28,3 @@ void* fileMemoryMap(int fd, size_t size, int flags) {
 void mappedMemoryFree(void* memory, size_t size) {
 	// TODO fill in
 }
-#else
-#include <sys/mman.h>
-
-void* anonymousMemoryMap(size_t size) {
-	return mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-}
-void* fileMemoryMap(int fd, size_t size, int flags) {
-	int mmapFlags = MAP_PRIVATE;
-	if (flags & MEMORY_WRITE) {
-		mmapFlags = MAP_SHARED;
-	}
-	return mmap(0, size, PROT_READ | PROT_WRITE, mmapFlags, fd, 0);
-}
-
-void mappedMemoryFree(void* memory, size_t size) {
-	munmap(memory, size);
-}
-#endif
