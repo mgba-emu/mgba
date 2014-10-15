38187bac7a5f1fca58f82f64af06881e1999dc68
Jeffrey Pfau <jeffrey@endrift.com>
2014-10-15 01:41:50 -0700

    Rename crc32 to doCrc32 to avoid symbol naming conflicts

--- src/gba/gba.c
+++ src/gba/gba.c
@@ -403,7 +403,7 @@ void GBALoadROM(struct GBA* gba, struct VFile* vf, struct VFile* sav, const char
 	gba->memory.rom = gba->pristineRom;
 	gba->activeFile = fname;
 	gba->memory.romSize = gba->pristineRomSize;
-	gba->romCrc32 = crc32(gba->memory.rom, gba->memory.romSize);
+	gba->romCrc32 = doCrc32(gba->memory.rom, gba->memory.romSize);
 	GBASavedataInit(&gba->memory.savedata, sav);
 	GBAGPIOInit(&gba->memory.gpio, &((uint16_t*) gba->memory.rom)[GPIO_REG_DATA >> 1]);
 	_checkOverrides(gba, ((struct GBACartridge*) gba->memory.rom)->id);
