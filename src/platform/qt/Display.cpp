6e62ba8bb260ab39ff31dbdf5e0e000c72ffaba2
Jeffrey Pfau <jeffrey@endrift.com>
2014-11-09 16:36:36 -0800

    Qt: Fix deadlock while telling GL thread to stop

--- src/platform/qt/Display.cpp
+++ src/platform/qt/Display.cpp
@@ -52,9 +52,13 @@ void Display::startDrawing(const uint32_t* buffer, GBAThread* thread) {
 
 void Display::stopDrawing() {
 	if (m_drawThread) {
+		GBAThreadInterrupt(m_context);
+		GBASyncSuspendDrawing(&m_context->sync);
 		QMetaObject::invokeMethod(m_painter, "stop", Qt::BlockingQueuedConnection);
 		m_drawThread->exit();
 		m_drawThread = nullptr;
+		GBASyncResumeDrawing(&m_context->sync);
+		GBAThreadContinue(m_context);
 	}
 }
 
