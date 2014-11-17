938c9e965d14e6867f165b3be3391b0c15484bf5
Jeffrey Pfau <jeffrey@endrift.com>
2014-11-16 20:25:52 -0800

    Util: Fix build with strndup on some platforms

--- src/util/string.h
+++ src/util/string.h
@@ -3,7 +3,11 @@
 
 #include "util/common.h"
 
+#ifndef strndup
+// This is sometimes a macro
 char* strndup(const char* start, size_t len);
+#endif
+
 char* strnrstr(const char* restrict s1, const char* restrict s2, size_t len);
 
 #endif
