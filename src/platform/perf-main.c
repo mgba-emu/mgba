09db378531bfe16c736f20cd62c79aa91d19b9d0
Jeffrey Pfau <jeffrey@endrift.com>
2015-01-25 03:12:53 -0800

    Perf: Fix crash when the GBA thread fails to start

--- src/platform/perf-main.c
+++ src/platform/perf-main.c
@@ -86,7 +86,11 @@ int main(int argc, char** argv) {
 	GBAMapArgumentsToContext(&args, &context);
 	GBAMapOptionsToContext(&opts, &context);
 
-	GBAThreadStart(&context);
+	int didStart = GBAThreadStart(&context);
+
+	if (!didStart) {
+		goto cleanup;
+	}
 	GBAGetGameCode(context.gba, gameCode);
 
 	int frames = perfOpts.frames;
@@ -102,12 +106,6 @@ int main(int argc, char** argv) {
 	uint64_t duration = end - start;
 
 	GBAThreadJoin(&context);
-	GBAConfigFreeOpts(&opts);
-	freeArguments(&args);
-	GBAConfigDeinit(&config);
-	free(context.debugger);
-
-	free(renderer.outputBuffer);
 
 	float scaledFrames = frames * 1000000.f;
 	if (perfOpts.csv) {
@@ -123,7 +121,14 @@ int main(int argc, char** argv) {
 		printf("%u frames in %" PRIu64 " microseconds: %g fps (%gx)\n", frames, duration, scaledFrames / duration, scaledFrames / (duration * 60.f));
 	}
 
-	return GBAThreadHasCrashed(&context);
+cleanup:
+	GBAConfigFreeOpts(&opts);
+	freeArguments(&args);
+	GBAConfigDeinit(&config);
+	free(context.debugger);
+	free(renderer.outputBuffer);
+
+	return !didStart || GBAThreadHasCrashed(&context);
 }
 
 static void _GBAPerfRunloop(struct GBAThread* context, int* frames, bool quiet) {
