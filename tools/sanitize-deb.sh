545497a7de4bb079e92274751daa46bc28e215b2
Jeffrey Pfau <jeffrey@endrift.com>
2015-02-14 01:44:48 -0800

    All: Add tool for cleaning up debs spat out by CPack

---
+++ tools/sanitize-deb.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# Clean up the caveats that CPack leaves behind
+BINARY=mgba
+
+rmdep () {
+    local DEP=$1
+    echo Removing dependency $DEP
+    sed -i~ "s/[^, ]*$DEP[^,]*//g" deb-temp/DEBIAN/control
+}
+
+adddep() {
+    local DEP=$1
+    echo Adding dependency $DEP
+    sed -i~ "s/^Depends: /&$DEP,/" deb-temp/DEBIAN/control
+}
+
+while [ $# -gt 0 ]; do
+    DEB=$1
+    dpkg-deb -R $DEB deb-temp
+    mv $DEB $DEB~
+    sed -i~ s/mgba-// deb-temp/DEBIAN/control
+    PKG=`head -n1 deb-temp/DEBIAN/control | cut -f2 -d ' '`
+    echo Found pacakge $PKG
+
+    case $PKG in
+    lib$BINARY)
+        rmdep sdl
+        rmdep qt
+    ;;
+    $BINARY-qt)
+        rmdep libav
+        rmdep libedit
+        rmdep libpng
+        rmdep libzip
+        rmdep libmagickwand
+        rmdep libswscale
+        rmdep zlib
+        adddep lib$BINARY
+    ;;
+    $BINARY-sdl)
+        rmdep libav
+        rmdep libedit
+        rmdep libpng
+        rmdep qt
+        rmdep libzip
+        rmdep libmagickwand
+        rmdep libswscale
+        rmdep zlib
+        adddep lib$BINARY
+    ;;
+    *)
+        echo Unknown package!
+    esac
+
+    sed -i~ "s/,,*/,/g" deb-temp/DEBIAN/control
+    sed -i~ "s/,$//g" deb-temp/DEBIAN/control
+    sed -i~ "/^[^:]*: $/d" deb-temp/DEBIAN/control
+    rm deb-temp/DEBIAN/control~
+    dpkg-deb -b deb-temp $DEB
+    shift
+done
