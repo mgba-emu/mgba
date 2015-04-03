767c6a039c13bf266c988239223fa5df97795c11
Jeffrey Pfau <jeffrey@endrift.com>
2015-04-02 22:32:38 -0700

    Qt: Pause game while open file dialogs are open (fixes #6 on GitHub)

--- src/platform/qt/Window.cpp
+++ src/platform/qt/Window.cpp
@@ -196,7 +196,14 @@ void Window::saveConfig() {
 }
 
 void Window::selectROM() {
+	bool doPause = m_controller->isLoaded() && !m_controller->isPaused();
+	if (doPause) {
+		m_controller->setPaused(true);
+	}
 	QString filename = QFileDialog::getOpenFileName(this, tr("Select ROM"), m_config->getQtOption("lastDirectory").toString(), tr("Game Boy Advance ROMs (*.gba *.zip *.rom *.bin)"));
+	if (doPause) {
+		m_controller->setPaused(false);
+	}
 	if (!filename.isEmpty()) {
 		m_config->setQtOption("lastDirectory", QFileInfo(filename).dir().path());
 		m_controller->loadGame(filename);
@@ -204,7 +211,14 @@ void Window::selectROM() {
 }
 
 void Window::selectBIOS() {
+	bool doPause = m_controller->isLoaded() && !m_controller->isPaused();
+	if (doPause) {
+		m_controller->setPaused(true);
+	}
 	QString filename = QFileDialog::getOpenFileName(this, tr("Select BIOS"), m_config->getQtOption("lastDirectory").toString());
+	if (doPause) {
+		m_controller->setPaused(false);
+	}
 	if (!filename.isEmpty()) {
 		m_config->setQtOption("lastDirectory", QFileInfo(filename).dir().path());
 		m_config->setOption("bios", filename);
@@ -216,7 +230,14 @@ void Window::selectBIOS() {
 }
 
 void Window::selectPatch() {
+	bool doPause = m_controller->isLoaded() && !m_controller->isPaused();
+	if (doPause) {
+		m_controller->setPaused(true);
+	}
 	QString filename = QFileDialog::getOpenFileName(this, tr("Select patch"), m_config->getQtOption("lastDirectory").toString(), tr("Patches (*.ips *.ups *.bps)"));
+	if (doPause) {
+		m_controller->setPaused(false);
+	}
 	if (!filename.isEmpty()) {
 		m_config->setQtOption("lastDirectory", QFileInfo(filename).dir().path());
 		m_controller->loadPatch(filename);
