b03ccf3b772a1e90f302b9d48e08fd23ffb7dd1f
Megumi Sonoda <sonodamegumi@outlook.com>
2015-04-14 15:00:29 -0500

    Fix typo

--- src/platform/qt/AboutDialog.h
+++ src/platform/qt/AboutDialog.h
@@ -3,7 +3,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#ifndef QBGA_ABOUT_DIALOG
+#ifndef QGBA_ABOUT_DIALOG
 #define QGBA_ABOUT_DIALOG
 #include <QWidget>
 #include "ui_AboutDialog.h"
