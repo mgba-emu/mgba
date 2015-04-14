04c19e102d775be00e01639c7ba9f18442e83f84
Megumi Sonoda <sonodamegumi@outlook.com>
2015-04-14 12:42:07 -0500

    Updates based on feedback

--- src/platform/qt/AboutDialog.cpp
+++ src/platform/qt/AboutDialog.cpp
@@ -5,14 +5,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "AboutDialog.h"
 
-#include <QDesktopServices>
-#include <QUrl>
-
 using namespace QGBA;
 
-AboutDialog::AboutDialog(QDialog* parent)
-	: QDialog(parent)
+AboutDialog::AboutDialog(QWidget* parent)
+	: QWidget(parent)
 {
 	m_ui.setupUi(this);
 	setFixedSize(size());
+	m_ui.projectName->setText(PROJECT_NAME);
+	m_ui.projectVersion->setText(PROJECT_VERSION);
 }
--- src/platform/qt/AboutDialog.h
+++ src/platform/qt/AboutDialog.h
@@ -3,18 +3,18 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#include <QDialog>
 #ifndef QBGA_ABOUT_DIALOG
 #define QGBA_ABOUT_DIALOG
+#include <QWidget>
 #include "ui_AboutDialog.h"
 
 namespace QGBA {
 
-class AboutDialog : public QDialog {
+class AboutDialog : public QWidget {
 Q_OBJECT
 
 public:
-	AboutDialog(QDialog* parent = nullptr);
+	AboutDialog(QWidget* parent = nullptr);
 
 private:
 	Ui::AboutDialog m_ui;
