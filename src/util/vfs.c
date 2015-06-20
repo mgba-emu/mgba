ef93f686580686e35044a0f620c37f3ddf850d18
Jeffrey Pfau <jeffrey@endrift.com>
2015-06-20 03:17:05 -0700

    VFS: Move VDirOptionalOpenFile back to vfs.c

--- src/util/vfs.c
+++ src/util/vfs.c
@@ -80,3 +80,31 @@ ssize_t VFileRead16LE(struct VFile* vf, void* hword) {
 	}
 	return r;
 }
+
+struct VFile* VDirOptionalOpenFile(struct VDir* dir, const char* realPath, const char* prefix, const char* suffix, int mode) {
+	char path[PATH_MAX];
+	path[PATH_MAX - 1] = '\0';
+	struct VFile* vf;
+	if (!dir) {
+		if (!realPath) {
+			return 0;
+		}
+		char* dotPoint = strrchr(realPath, '.');
+		if (dotPoint - realPath + 1 >= PATH_MAX - 1) {
+			return 0;
+		}
+		if (dotPoint > strrchr(realPath, '/')) {
+			int len = dotPoint - realPath;
+			strncpy(path, realPath, len);
+			path[len] = 0;
+			strncat(path + len, suffix, PATH_MAX - len - 1);
+		} else {
+			snprintf(path, PATH_MAX - 1, "%s%s", realPath, suffix);
+		}
+		vf = VFileOpen(path, mode);
+	} else {
+		snprintf(path, PATH_MAX - 1, "%s%s", prefix, suffix);
+		vf = dir->openFile(dir, path, mode);
+	}
+	return vf;
+}
