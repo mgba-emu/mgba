c0d7cfbf0b99e4cf6e28a0a2b9727e5fc2a453ab
Jeffrey Pfau <jeffrey@endrift.com>
2015-08-30 22:54:53 -0700

    GBA Audio: Allow GBAAVStream to have no video callback

--- src/gba/gba.c
+++ src/gba/gba.c
@@ -812,7 +812,7 @@ void GBAFrameEnded(struct GBA* gba) {
 		}
 	}
 
-	if (gba->stream) {
+	if (gba->stream && gba->stream->postVideoFrame) {
 		gba->stream->postVideoFrame(gba->stream, gba->video.renderer);
 	}
 
