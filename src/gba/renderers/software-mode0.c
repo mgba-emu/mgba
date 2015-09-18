cf7b100f014b79f1dcae3ba6106d0faad4be2812
Jeffrey Pfau <jeffrey@endrift.com>
2015-09-17 19:47:29 -0700

    GBA Video: Restore a fixed version of a removed optimization

--- src/gba/renderers/software-mode0.c
+++ src/gba/renderers/software-mode0.c
@@ -473,6 +473,10 @@ void GBAVideoSoftwareRendererDrawBackgroundMode0(struct GBAVideoSoftwareRenderer
 	int objwinFlags = FLAG_TARGET_1 * (background->target1 && renderer->blendEffect == BLEND_ALPHA && GBAWindowControlIsBlendEnable(renderer->objwin.packed));
 	objwinFlags |= flags;
 	flags |= FLAG_TARGET_1 * (background->target1 && renderer->blendEffect == BLEND_ALPHA && GBAWindowControlIsBlendEnable(renderer->currentWindow.packed));
+	if (renderer->blendEffect == BLEND_ALPHA && renderer->blda == 0x10 && renderer->bldb == 0) {
+		flags &= ~(FLAG_TARGET_1 | FLAG_TARGET_2);
+		objwinFlags &= ~(FLAG_TARGET_1 | FLAG_TARGET_2);
+	}
 
 	uint32_t screenBase;
 	uint32_t charBase;
