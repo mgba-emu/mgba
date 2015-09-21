8438797d53c3135319dbc7bb0711fe11f8ede0e0
Jeffrey Pfau <jeffrey@endrift.com>
2015-09-21 00:53:27 -0700

    All: Travis attempt 4

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ $TRAVIS_OS_NAME -eq "osx" ]; then
+if [ $TRAVIS_OS_NAME = "osx" ]; then
 	brew install qt5 ffmpeg imagemagick sdl2 libzip libpng
 else
 	sudo add-apt-repository ppa:smspillaz/cmake-2.8.12 -y
@@ -9,7 +9,7 @@ else
 	sudo apt-get update -qq
 	sudo apt-get purge cmake -qq
 	sudo apt-get install -y -qq cmake libedit-dev libmagickwand-dev \
-		libpng-devlibsdl2-dev libzip-dev qtbase5-dev \
+		libpng-dev libsdl2-dev libzip-dev qtbase5-dev \
 		libqt5opengl5-dev qtmultimedia5-dev libavcodec-dev \
 		libavutil-dev libavformat-dev libavresample-dev libswscale-dev
 fi
