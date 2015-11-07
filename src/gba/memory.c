a9956087bceb382af890d7abaed6f03da5e9e0d6
Jeffrey Pfau <jeffrey@endrift.com>
2015-11-06 21:08:29 -0800

    GBA Memory: Fix alignment of LDM/STM on SRAM

--- src/gba/memory.c
+++ src/gba/memory.c
@@ -1111,7 +1111,9 @@ uint32_t GBALoadMultiple(struct ARMCore* cpu, uint32_t address, int mask, enum L
 	}
 
 	uint32_t addressMisalign = address & 0x3;
-	address &= 0xFFFFFFFC;
+	if (address >> BASE_OFFSET < REGION_CART_SRAM) {
+		address &= 0xFFFFFFFC;
+	}
 
 	switch (address >> BASE_OFFSET) {
 	case REGION_BIOS:
@@ -1224,7 +1226,9 @@ uint32_t GBAStoreMultiple(struct ARMCore* cpu, uint32_t address, int mask, enum 
 	}
 
 	uint32_t addressMisalign = address & 0x3;
-	address &= 0xFFFFFFFC;
+	if (address >> BASE_OFFSET < REGION_CART_SRAM) {
+		address &= 0xFFFFFFFC;
+	}
 
 	switch (address >> BASE_OFFSET) {
 	case REGION_WORKING_RAM:
