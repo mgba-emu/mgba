7253e5191b4c3c8f05b5da4c12d7ffce954a98f7
twinaphex <libretro@gmail.com>
2015-11-17 05:20:16 +0100

    Set audio samples to 128 for Wii/Gamecube

--- src/platform/libretro/libretro.c
+++ src/platform/libretro/libretro.c
@@ -24,7 +24,11 @@
 FS_archive sdmcArchive;
 #endif
 
+#ifdef GEKKO
+#define SAMPLES 128
+#else
 #define SAMPLES 1024
+#endif
 #define RUMBLE_PWM 35
 
 #define SOLAR_SENSOR_LEVEL "mgba_solar_sensor_level"
