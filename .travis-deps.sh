ef4995d74e4291c86395c3c0d45cef2ec41e16f3
Jeffrey Pfau <jeffrey@endrift.com>
2016-02-14 11:47:24 -0800

    All: Travis trusty upgrade attempt 3

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -3,7 +3,6 @@ if [ $TRAVIS_OS_NAME = "osx" ]; then
 	brew update
 	brew install qt5 ffmpeg imagemagick sdl2 libzip libpng
 else
-	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	sudo apt-get update -qq
 	sudo apt-get purge cmake -qq
 	sudo apt-get install -y -qq cmake libedit-dev libmagickwand-dev \
