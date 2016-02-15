8061bbfd96bc6bc79cdbd260b7e25ea1e42f46b6
Jeffrey Pfau <jeffrey@endrift.com>
2016-02-15 01:04:29 -0800

    GBA BIOS: Fix RegisterRamReset setting DISPCNT to the wrong value

--- src/gba/bios.c
+++ src/gba/bios.c
@@ -88,7 +88,6 @@ static void _RegisterRamReset(struct GBA* gba) {
 		memset(gba->audio.ch3.wavedata, 0, sizeof(gba->audio.ch3.wavedata));
 	}
 	if (registers & 0x80) {
-		cpu->memory.store16(cpu, BASE_IO | 0x00, 0, 0);
 		cpu->memory.store16(cpu, BASE_IO | 0x04, 0, 0);
 		cpu->memory.store16(cpu, BASE_IO | 0x06, 0, 0);
 		cpu->memory.store16(cpu, BASE_IO | 0x08, 0, 0);
