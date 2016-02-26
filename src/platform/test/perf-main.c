9f6837da422c9f7b1a83e6ace63b23a2a8f1e393
Jeffrey Pfau <jeffrey@endrift.com>
2016-02-25 23:54:14 -0800

    Perf: De-threadify and make compatible with 3DS

--- src/platform/test/perf-main.c
+++ src/platform/test/perf-main.c
@@ -4,7 +4,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "core/config.h"
-#include "core/thread.h"
 #include "gba/core.h"
 #include "gba/gba.h"
 #include "gba/renderers/video-software.h"
@@ -13,6 +12,10 @@
 #include "platform/commandline.h"
 #include "util/string.h"
 #include "util/vfs.h"
+
+#ifdef _3DS
+#include <3ds.h>
+#endif
 
 #include <errno.h>
 #include <fcntl.h>
@@ -37,17 +40,40 @@ struct PerfOpts {
 	char* savestate;
 };
 
-static void _GBAPerfRunloop(struct mCoreThread* context, int* frames, bool quiet);
-static void _GBAPerfShutdown(int signal);
-static bool _parsePerfOpts(struct mSubParser* parser, int option, const char* arg);
-static void _loadSavestate(struct mCoreThread* context);
+#ifdef _3DS
+extern bool allocateRomBuffer(void);
+FS_Archive sdmcArchive;
+#endif
 
-static struct mCoreThread* _thread;
+static void _mPerfRunloop(struct mCore* context, int* frames, bool quiet);
+static void _mPerfShutdown(int signal);
+static bool _parsePerfOpts(struct mSubParser* parser, int option, const char* arg);
+static void _log(struct mLogger*, int, enum mLogLevel, const char*, va_list);
+
 static bool _dispatchExiting = false;
 static struct VFile* _savestate = 0;
 
 int main(int argc, char** argv) {
-	signal(SIGINT, _GBAPerfShutdown);
+#ifdef _3DS
+    gfxInitDefault();
+    osSetSpeedupEnable(true);
+	consoleInit(GFX_BOTTOM, NULL);
+	if (!allocateRomBuffer()) {
+		return 1;
+	}
+	sdmcArchive = (FS_Archive) {
+		ARCHIVE_SDMC,
+		(FS_Path) { PATH_EMPTY, 1, "" },
+		0
+	};
+	FSUSER_OpenArchive(&sdmcArchive);
+#else
+	signal(SIGINT, _mPerfShutdown);
+#endif
+	int didFail = 0;
+
+	struct mLogger logger = { .log = _log };
+	mLogSetDefaultLogger(&logger);
 
 	struct PerfOpts perfOpts = { false, false, 0, 0, 0 };
 	struct mSubParser subparser = {
@@ -57,30 +83,26 @@ int main(int argc, char** argv) {
 		.opts = &perfOpts
 	};
 
-	struct mArguments args;
+	struct mArguments args = {};
 	bool parsed = parseArguments(&args, argc, argv, &subparser);
 	if (!parsed || args.showHelp) {
 		usage(argv[0], PERF_USAGE);
-		freeArguments(&args);
-		return !parsed;
+		didFail = !parsed;
+		goto cleanup;
 	}
+
 	if (args.showVersion) {
 		version(argv[0]);
-		freeArguments(&args);
-		return 0;
+		goto cleanup;
 	}
 
 	void* outputBuffer = malloc(256 * 256 * 4);
 
 	struct mCore* core = mCoreFind(args.fname);
 	if (!core) {
-		freeArguments(&args);
-		return 1;
+		didFail = 1;
+		goto cleanup;
 	}
-	struct mCoreThread context = {
-		.core = core
-	};
-	_thread = &context;
 
 	if (!perfOpts.noVideo) {
 		core->setVideoBuffer(core, outputBuffer, 256);
@@ -88,9 +110,6 @@ int main(int argc, char** argv) {
 	if (perfOpts.savestate) {
 		_savestate = VFileOpen(perfOpts.savestate, O_RDONLY);
 		free(perfOpts.savestate);
-	}
-	if (_savestate) {
-		context.startCallback = _loadSavestate;
 	}
 
 	// TODO: Put back debugger
@@ -110,19 +129,14 @@ int main(int argc, char** argv) {
 	mCoreConfigLoadDefaults(&core->config, &opts);
 	mCoreLoadConfig(core);
 
-	int didStart = mCoreThreadStart(&context);
-
-	if (!didStart) {
-		goto cleanup;
-	}
-	mCoreThreadInterrupt(&context);
-	if (mCoreThreadHasCrashed(&context)) {
-		mCoreThreadJoin(&context);
-		goto cleanup;
+	core->reset(core);
+	if (_savestate) {
+		core->loadState(core, _savestate, 0);
+		_savestate->close(_savestate);
+		_savestate = NULL;
 	}
 
-	GBAGetGameCode(core->board, gameCode);
-	mCoreThreadContinue(&context);
+	core->getGameCode(core, gameCode);
 
 	int frames = perfOpts.frames;
 	if (!frames) {
@@ -131,12 +145,12 @@ int main(int argc, char** argv) {
 	struct timeval tv;
 	gettimeofday(&tv, 0);
 	uint64_t start = 1000000LL * tv.tv_sec + tv.tv_usec;
-	_GBAPerfRunloop(&context, &frames, perfOpts.csv);
+	_mPerfRunloop(core, &frames, perfOpts.csv);
 	gettimeofday(&tv, 0);
 	uint64_t end = 1000000LL * tv.tv_sec + tv.tv_usec;
 	uint64_t duration = end - start;
 
-	mCoreThreadJoin(&context);
+	core->deinit(core);
 
 	float scaledFrames = frames * 1000000.f;
 	if (perfOpts.csv) {
@@ -152,49 +166,46 @@ int main(int argc, char** argv) {
 		printf("%u frames in %" PRIu64 " microseconds: %g fps (%gx)\n", frames, duration, scaledFrames / duration, scaledFrames / (duration * 60.f));
 	}
 
-cleanup:
-	if (_savestate) {
-		_savestate->close(_savestate);
-	}
 	mCoreConfigFreeOpts(&opts);
-	freeArguments(&args);
 	mCoreConfigDeinit(&core->config);
 	free(outputBuffer);
 
-	return !didStart || mCoreThreadHasCrashed(&context);
+	cleanup:
+	freeArguments(&args);
+
+#ifdef _3DS
+	gfxExit();
+#endif
+
+	return didFail;
 }
 
-static void _GBAPerfRunloop(struct mCoreThread* context, int* frames, bool quiet) {
+static void _mPerfRunloop(struct mCore* core, int* frames, bool quiet) {
 	struct timeval lastEcho;
 	gettimeofday(&lastEcho, 0);
 	int duration = *frames;
 	*frames = 0;
 	int lastFrames = 0;
-	while (context->state < THREAD_EXITING) {
-		if (mCoreSyncWaitFrameStart(&context->sync)) {
-			++*frames;
-			++lastFrames;
-			if (!quiet) {
-				struct timeval currentTime;
-				long timeDiff;
-				gettimeofday(&currentTime, 0);
-				timeDiff = currentTime.tv_sec - lastEcho.tv_sec;
-				timeDiff *= 1000;
-				timeDiff += (currentTime.tv_usec - lastEcho.tv_usec) / 1000;
-				if (timeDiff >= 1000) {
-					printf("\033[2K\rCurrent FPS: %g (%gx)", lastFrames / (timeDiff / 1000.0f), lastFrames / (float) (60 * (timeDiff / 1000.0f)));
-					fflush(stdout);
-					lastEcho = currentTime;
-					lastFrames = 0;
-				}
+	while (!_dispatchExiting) {
+		core->runFrame(core);
+		++*frames;
+		++lastFrames;
+		if (!quiet) {
+			struct timeval currentTime;
+			long timeDiff;
+			gettimeofday(&currentTime, 0);
+			timeDiff = currentTime.tv_sec - lastEcho.tv_sec;
+			timeDiff *= 1000;
+			timeDiff += (currentTime.tv_usec - lastEcho.tv_usec) / 1000;
+			if (timeDiff >= 1000) {
+				printf("\033[2K\rCurrent FPS: %g (%gx)", lastFrames / (timeDiff / 1000.0f), lastFrames / (float) (60 * (timeDiff / 1000.0f)));
+				fflush(stdout);
+				lastEcho = currentTime;
+				lastFrames = 0;
 			}
 		}
-		mCoreSyncWaitFrameEnd(&context->sync);
 		if (duration > 0 && *frames == duration) {
-			_GBAPerfShutdown(0);
-		}
-		if (_dispatchExiting) {
-			mCoreThreadEnd(context);
+			break;
 		}
 	}
 	if (!quiet) {
@@ -202,11 +213,9 @@ static void _GBAPerfRunloop(struct mCoreThread* context, int* frames, bool quiet
 	}
 }
 
-static void _GBAPerfShutdown(int signal) {
+static void _mPerfShutdown(int signal) {
 	UNUSED(signal);
-	// This will come in ON the GBA thread, so we have to handle it carefully
 	_dispatchExiting = true;
-	ConditionWake(&_thread->sync.videoFrameAvailableCond);
 }
 
 static bool _parsePerfOpts(struct mSubParser* parser, int option, const char* arg) {
@@ -233,8 +242,10 @@ static bool _parsePerfOpts(struct mSubParser* parser, int option, const char* ar
 	}
 }
 
-static void _loadSavestate(struct mCoreThread* context) {
-	context->core->loadState(context->core, _savestate, 0);
-	_savestate->close(_savestate);
-	_savestate = 0;
+static void _log(struct mLogger* log, int category, enum mLogLevel level, const char* format, va_list args) {
+	UNUSED(log);
+	UNUSED(category);
+	UNUSED(level);
+	UNUSED(format);
+	UNUSED(args);
 }
