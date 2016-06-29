59821adb69c63e436dceb783bcdf11e7210ec306
Jeffrey Pfau <jeffrey@endrift.com>
2016-06-28 22:34:43 -0700

    GBA: Fix some warnings from a bad merge

--- src/gba/context/overrides.c
+++ src/gba/context/overrides.c
@@ -33,9 +33,9 @@ static const struct GBACartridgeOverride _overrides[] = {
 	{ "ALGP", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE },
 
 	// Dragon Ball Z - The Legacy of Goku II
-	{ "ALFJ", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE, false },
-	{ "ALFE", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE, false },
-	{ "ALFP", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE, false },
+	{ "ALFJ", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE },
+	{ "ALFE", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE },
+	{ "ALFP", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE },
 
 	// Dragon Ball Z - Taiketsu
 	{ "BDBE", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE },
@@ -157,7 +157,7 @@ static const struct GBACartridgeOverride _overrides[] = {
 	{ "A2YE", SAVEDATA_FORCE_NONE, HW_NONE, IDLE_LOOP_NONE },
 
 	// Ueki no Housoku - Jingi Sakuretsu! Nouryokusha Battle
-	{ "BUHJ", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE, false },
+	{ "BUHJ", SAVEDATA_EEPROM, HW_NONE, IDLE_LOOP_NONE },
 
 	// Wario Ware Twisted
 	{ "RZWJ", SAVEDATA_SRAM, HW_RUMBLE | HW_GYRO, IDLE_LOOP_NONE },
