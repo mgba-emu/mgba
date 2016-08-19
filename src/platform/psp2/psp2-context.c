026f5233db5a96707d2b4a06f0b22747f0b16b71
Jeffrey Pfau <jeffrey@endrift.com>
2016-08-19 03:30:48 -0700

    PSP2: Use sceDisplayGetRefreshRate to get ideal frame rate

--- src/platform/psp2/psp2-context.c
+++ src/platform/psp2/psp2-context.c
@@ -218,7 +218,9 @@ void mPSP2Setup(struct mGUIRunner* runner) {
 
 void mPSP2LoadROM(struct mGUIRunner* runner) {
 	scePowerSetArmClockFrequency(444);
-	double ratio = GBAAudioCalculateRatio(1, 60.0f / 1.001f, 1);
+	float rate = 60.0f / 1.001f;
+	sceDisplayGetRefreshRate(&rate);
+	double ratio = GBAAudioCalculateRatio(1, rate, 1);
 	blip_set_rates(runner->core->getAudioChannel(runner->core, 0), runner->core->frequency(runner->core), 48000 * ratio);
 	blip_set_rates(runner->core->getAudioChannel(runner->core, 1), runner->core->frequency(runner->core), 48000 * ratio);
 
