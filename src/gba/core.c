a43c465f9fe18b5959121b0fe6dd5cfc358e83c2
Jeffrey Pfau <jeffrey@endrift.com>
2016-08-24 12:55:25 -0700

    GBA: Fall back to idle loop remove mode if already present

--- src/gba/core.c
+++ src/gba/core.c
@@ -134,7 +134,11 @@ static void _GBACoreLoadConfig(struct mCore* core, const struct mCoreConfig* con
 		} else if (strcasecmp(idleOptimization, "remove") == 0) {
 			gba->idleOptimization = IDLE_LOOP_REMOVE;
 		} else if (strcasecmp(idleOptimization, "detect") == 0) {
-			gba->idleOptimization = IDLE_LOOP_DETECT;
+			if (gba->idleLoop == IDLE_LOOP_NONE) {
+				gba->idleOptimization = IDLE_LOOP_DETECT;
+			} else {
+				gba->idleOptimization = IDLE_LOOP_REMOVE;
+			}
 		}
 	}
 
