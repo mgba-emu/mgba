47e4b0f460287149fafb5d69f9d37996cab7f608
MerryMage <MerryMage@users.noreply.github.com>
2016-09-08 21:48:34 +0100

    dynarec/emitter: Add more instructions

--- src/arm/dynarec-arm/emitter.c
+++ src/arm/dynarec-arm/emitter.c
@@ -3,37 +3,54 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#include <assert.h>
+
 #include "arm/decoder.h"
 #include "arm/dynarec.h"
 #include "arm/isa-thumb.h"
 #include "arm/dynarec-arm/emitter.h"
 
-#define OP_I     0x02000000
-#define OP_S     0x00100000
-#define OP_ADD   0x00800000
-#define OP_AND   0x00000000
-#define OP_B     0x0A000000
-#define OP_BIC   0x01C00000
-#define OP_BL    0x0B000000
-#define OP_CMP   0x01500000
-#define OP_EOR   0x00200000
-#define OP_LDMIA 0x08900000
-#define OP_LDRI  0x05100000
-#define OP_MOV   0x01A00000
-#define OP_MOVT  0x03400000
-#define OP_MOVW  0x03000000
+#define OP_I       0x02000000
+#define OP_S       0x00100000
+#define ADDR1_ASR  0x00000040
+#define ADDR1_LSL  0x00000000
+#define ADDR1_LSR  0x00000020
+#define ADDR1_ROR  0x00000060
+
+#define OP_ADC     0x00A00000
+#define OP_ADD     0x00800000
+#define OP_AND     0x00000000
+#define OP_BIC     0x01C00000
+#define OP_CMN     0x01700000
+#define OP_CMP     0x01500000
+#define OP_EOR     0x00200000
+#define OP_MOV     0x01A00000
+#define OP_MOVT    0x03400000
+#define OP_MOVW    0x03000000
+#define OP_MVN     0x01E00000
+#define OP_ORR     0x01800000
+#define OP_RSB     0x00600000
+#define OP_RSC     0x00E00000
+#define OP_SBC     0x00C00000
+#define OP_SUB     0x00400000
+#define OP_TEQ     0x01300000
+#define OP_TST     0x01100000
+
+#define OP_LDMIA   0x08900000
+#define OP_LDRI    0x05100000
+#define OP_POP     0x08BD0000
+#define OP_PUSH    0x092D0000
+#define OP_STMIA   0x08800000
+#define OP_STRI    0x05000000
+#define OP_STRBI   0x05400000
+
+#define OP_B       0x0A000000
+#define OP_BL      0x0B000000
+
 #define OP_MRS   0x010F0000
-#define OP_ORR   0x01800000
-#define OP_POP   0x08BD0000
-#define OP_PUSH  0x092D0000
-#define OP_STMIA 0x08800000
-#define OP_STRI  0x05000000
-#define OP_STRBI 0x05400000
-#define OP_SUB   0x00400000
+#define OP_MSR   0x0120F000
 
-#define ADDR1_LSRI 0x00000020
-
-uint32_t calculateAddrMode1(unsigned imm) {
+static uint32_t calculateAddrMode1(unsigned imm) {
 	if (imm < 0x100) {
 		return imm;
 	}
@@ -47,70 +64,86 @@ uint32_t calculateAddrMode1(unsigned imm) {
 	abort();
 }
 
-uint32_t emitADDI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_ADD | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
+#define SHIFT(MN, S, I, ARGS, BODY) \
+	uint32_t emit##MN##S##_ASR##I ARGS { return OP_##MN | ADDR1_ASR | BODY; } \
+	uint32_t emit##MN##S##_LSL##I ARGS { return OP_##MN | ADDR1_LSL | BODY; } \
+	uint32_t emit##MN##S##_LSR##I ARGS { return OP_##MN | ADDR1_LSR | BODY; } \
+	uint32_t emit##MN##S##_ROR##I ARGS { return OP_##MN | ADDR1_ROR | BODY; }
+
+#define DEFINE_ALU3_EMITTER(MN) \
+	uint32_t emit##MN(unsigned dst, unsigned src, unsigned op2) { \
+		return OP_##MN | (dst << 12) | (src << 16) | op2; \
+	} \
+	uint32_t emit##MN##I(unsigned dst, unsigned src, unsigned imm) { \
+		return OP_##MN | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16); \
+	} \
+	uint32_t emit##MN##S(unsigned dst, unsigned src, unsigned op2) { \
+		return OP_##MN | OP_S | (dst << 12) | (src << 16) | op2; \
+	} \
+	uint32_t emit##MN##SI(unsigned dst, unsigned src, unsigned imm) { \
+		return OP_##MN | OP_S | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16); \
+	} \
+	SHIFT(MN,  ,  , (unsigned dst, unsigned src1, unsigned src2, unsigned src3), (       (dst << 12) | (src1 << 16) | (src3 << 8) | src2)) \
+	SHIFT(MN, S,  , (unsigned dst, unsigned src1, unsigned src2, unsigned src3), (OP_S | (dst << 12) | (src1 << 16) | (src3 << 8) | src2)) \
+	SHIFT(MN,  , I, (unsigned dst, unsigned src1, unsigned src2, unsigned imm), (       (dst << 12) | (src1 << 16) | ((imm & 0x1F) << 7) | src2)) \
+	SHIFT(MN, S, I, (unsigned dst, unsigned src1, unsigned src2, unsigned imm), (OP_S | (dst << 12) | (src1 << 16) | ((imm & 0x1F) << 7) | src2))
+
+#define DEFINE_ALU2_EMITTER(MN) \
+	uint32_t emit##MN(unsigned dst, unsigned op2) { \
+		return OP_##MN | (dst << 12) | op2; \
+	} \
+	uint32_t emit##MN##I(unsigned dst, unsigned imm) { \
+		return OP_##MN | OP_I | calculateAddrMode1(imm) | (dst << 12); \
+	} \
+	uint32_t emit##MN##S(unsigned dst, unsigned op2) { \
+		return OP_##MN | OP_S | (dst << 12) | op2; \
+	} \
+	uint32_t emit##MN##SI(unsigned dst, unsigned imm) { \
+		return OP_##MN | OP_S | OP_I | calculateAddrMode1(imm) | (dst << 12); \
+	} \
+	SHIFT(MN,  ,  , (unsigned dst, unsigned src1, unsigned src2), (       (dst << 12) | (src2 << 8) | src1)) \
+	SHIFT(MN, S,  , (unsigned dst, unsigned src1, unsigned src2), (OP_S | (dst << 12) | (src2 << 8) | src1)) \
+	SHIFT(MN,  , I, (unsigned dst, unsigned src1, unsigned imm), (       (dst << 12) | ((imm & 0x1F) << 7) | src1)) \
+	SHIFT(MN, S, I, (unsigned dst, unsigned src1, unsigned imm), (OP_S | (dst << 12) | ((imm & 0x1F) << 7) | src1))
+
+#define DEFINE_ALU1_EMITTER(MN) \
+	uint32_t emit##MN(unsigned src, unsigned op2) { \
+		return OP_##MN | (src << 16) | op2; \
+	} \
+	uint32_t emit##MN##I(unsigned src, unsigned imm) { \
+		return OP_##MN | OP_I | calculateAddrMode1(imm) | (src << 16); \
+	} \
+	SHIFT(MN,  ,  , (unsigned src1, unsigned src2, unsigned src3), ((src1 << 16) | (src3 << 8) | src2)) \
+	SHIFT(MN,  , I, (unsigned src1, unsigned src2, unsigned imm), ((src1 << 16) | ((imm & 0x1F) << 7) | src2))
+
+DEFINE_ALU3_EMITTER(ADC)
+DEFINE_ALU3_EMITTER(ADD)
+DEFINE_ALU3_EMITTER(AND)
+DEFINE_ALU3_EMITTER(BIC)
+DEFINE_ALU1_EMITTER(CMN)
+DEFINE_ALU1_EMITTER(CMP)
+DEFINE_ALU3_EMITTER(EOR)
+DEFINE_ALU2_EMITTER(MOV)
+DEFINE_ALU2_EMITTER(MVN)
+DEFINE_ALU3_EMITTER(ORR)
+DEFINE_ALU3_EMITTER(RSB)
+DEFINE_ALU3_EMITTER(RSC)
+DEFINE_ALU3_EMITTER(SBC)
+DEFINE_ALU3_EMITTER(SUB)
+DEFINE_ALU1_EMITTER(TEQ)
+DEFINE_ALU1_EMITTER(TST)
+
+#undef DEFINE_ALU3_EMITTER
+#undef DEFINE_ALU2_EMITTER
+#undef DEFINE_ALU1_EMITTER
+#undef SHIFT
+
+uint32_t emitMOVT(unsigned dst, uint16_t value) {
+	return OP_MOVT | (dst << 12) | ((value & 0xF000) << 4) | (value & 0x0FFF);
 }
 
-uint32_t emitADDS(unsigned dst, unsigned src, unsigned op2) {
-	return OP_ADD | OP_S | (dst << 12) | (src << 16) | op2;
-}
-
-uint32_t emitADDSI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_ADD | OP_S | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
-}
-
-uint32_t emitANDI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_AND | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
-}
-
-uint32_t emitANDS(unsigned dst, unsigned src, unsigned op2) {
-	return OP_AND | OP_S | (dst << 12) | (src << 16) | op2;
-}
-
-uint32_t emitANDSI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_AND | OP_S | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
-}
-
-uint32_t emitBICI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_BIC | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
-}
-
-uint32_t emitBICS(unsigned dst, unsigned src, unsigned op2) {
-	return OP_BIC | OP_S | (dst << 12) | (src << 16) | op2;
-}
-
-uint32_t emitBICSI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_BIC | OP_S | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
-}
-
-uint32_t emitB(void* base, void* target) {
-	uint32_t diff = (intptr_t) target - (intptr_t) base - WORD_SIZE_ARM * 2;
-	diff >>= 2;
-	diff &= 0x00FFFFFF;
-	return OP_B | diff;
-}
-
-uint32_t emitBL(void* base, void* target) {
-	uint32_t diff = (intptr_t) target - (intptr_t) base - WORD_SIZE_ARM * 2;
-	diff >>= 2;
-	diff &= 0x00FFFFFF;
-	return OP_BL | diff;
-}
-
-uint32_t emitCMP(unsigned src1, unsigned src2) {
-	return OP_CMP | src2 | (src1 << 16);
-}
-
-uint32_t emitEORI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_EOR | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
-}
-
-uint32_t emitEORS(unsigned dst, unsigned src, unsigned op2) {
-	return OP_EOR | OP_S | (dst << 12) | (src << 16) | op2;
-}
-
-uint32_t emitEORSI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_EOR | OP_S | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
+uint32_t emitMOVW(unsigned dst, uint16_t value) {
+	return OP_MOVW | (dst << 12) | ((value & 0xF000) << 4) | (value & 0x0FFF);
 }
 
 uint32_t emitLDMIA(unsigned base, unsigned mask) {
@@ -125,38 +158,6 @@ uint32_t emitLDRI(unsigned reg, unsigned base, int offset) {
 		op |= -offset & 0xFFF;
 	}
 	return op;
-}
-
-uint32_t emitMOV(unsigned dst, unsigned src) {
-	return OP_MOV | (dst << 12) | src;
-}
-
-uint32_t emitMOV_LSRI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_MOV | ADDR1_LSRI | (dst << 12) | ((imm  & 0x1F) << 7) | src;
-}
-
-uint32_t emitMOVT(unsigned dst, uint16_t value) {
-	return OP_MOVT | (dst << 12) | ((value & 0xF000) << 4) | (value & 0x0FFF);
-}
-
-uint32_t emitMOVW(unsigned dst, uint16_t value) {
-	return OP_MOVW | (dst << 12) | ((value & 0xF000) << 4) | (value & 0x0FFF);
-}
-
-uint32_t emitMRS(unsigned dst) {
-	return OP_MRS | (dst << 12);
-}
-
-uint32_t emitORRI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_ORR | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
-}
-
-uint32_t emitORRS(unsigned dst, unsigned src, unsigned op2) {
-	return OP_ORR | OP_S | (dst << 12) | (src << 16) | op2;
-}
-
-uint32_t emitORRSI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_ORR | OP_S | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
 }
 
 uint32_t emitPOP(unsigned mask) {
@@ -191,55 +192,24 @@ uint32_t emitSTRBI(unsigned reg, unsigned base, int offset) {
 	return op;
 }
 
-uint32_t emitSUBI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_SUB | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
+uint32_t emitB(void* base, void* target) {
+	uint32_t diff = (intptr_t) target - (intptr_t) base - WORD_SIZE_ARM * 2;
+	diff >>= 2;
+	diff &= 0x00FFFFFF;
+	return OP_B | diff;
 }
 
-uint32_t emitSUBS(unsigned dst, unsigned src1, unsigned src2) {
-	return OP_SUB | OP_S | (dst << 12) | (src1 << 16) | src2;
+uint32_t emitBL(void* base, void* target) {
+	uint32_t diff = (intptr_t) target - (intptr_t) base - WORD_SIZE_ARM * 2;
+	diff >>= 2;
+	diff &= 0x00FFFFFF;
+	return OP_BL | diff;
 }
 
-uint32_t emitSUBSI(unsigned dst, unsigned src, unsigned imm) {
-	return OP_SUB | OP_S | OP_I | calculateAddrMode1(imm) | (dst << 12) | (src << 16);
+uint32_t emitMRS(unsigned dst) {
+	return OP_MRS | (dst << 12);
 }
 
-void updatePC(struct ARMDynarecContext* ctx, uint32_t address) {
-	EMIT_IMM(ctx, AL, 5, address);
-	EMIT(ctx, STRI, AL, 5, 4, ARM_PC * sizeof(uint32_t));
-}
-
-void updateEvents(struct ARMDynarecContext* ctx, struct ARMCore* cpu) {
-	EMIT(ctx, ADDI, AL, 0, 4, offsetof(struct ARMCore, cycles));
-	EMIT(ctx, LDMIA, AL, 0, 6);
-	EMIT(ctx, SUBS, AL, 0, 2, 1);
-	EMIT(ctx, MOV, AL, 0, 4);
-	EMIT(ctx, BL, LE, ctx->code, cpu->irqh.processEvents);
-	EMIT(ctx, LDRI, AL, 1, 4, ARM_PC * sizeof(uint32_t));
-	EMIT(ctx, CMP, AL, 1, 5);
-	EMIT(ctx, POP, NE, 0x8030);
-}
-
-void flushPrefetch(struct ARMDynarecContext* ctx, uint32_t op0, uint32_t op1) {
-	EMIT_IMM(ctx, AL, 1, op0);
-	EMIT_IMM(ctx, AL, 2, op1);
-	EMIT(ctx, ADDI, AL, 0, 4, offsetof(struct ARMCore, prefetch));
-	EMIT(ctx, STMIA, AL, 0, 6);
-}
-
-void loadReg(struct ARMDynarecContext* ctx, unsigned emureg, unsigned sysreg) {
-	EMIT(ctx, LDRI, AL, sysreg, 4, emureg * sizeof(uint32_t)); 
-}
-
-void flushReg(struct ARMDynarecContext* ctx, unsigned emureg, unsigned sysreg) {
-	EMIT(ctx, STRI, AL, sysreg, 4, emureg * sizeof(uint32_t)); 
-}
-
-void flushCycles(struct ARMDynarecContext* ctx) {
-	if (ctx->cycles == 0) {
-		return;
-	}
-	EMIT(ctx, LDRI, AL, 0, 4, offsetof(struct ARMCore, cycles)); 
-	EMIT(ctx, ADDI, AL, 0, 0, ctx->cycles);
-	EMIT(ctx, STRI, AL, 0, 4, offsetof(struct ARMCore, cycles));
-	ctx->cycles = 0;
+uint32_t emitMSR(bool nzcvq, bool g, unsigned src) {
+    return OP_MSR | (nzcvq << 19) | (g << 18) | src;
 }
