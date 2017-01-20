d8687d32c49e75c5f665df10a92d1b0a9e10687f
Jeffrey Pfau <jeffrey@endrift.com>
2017-01-20 15:26:41 -0800

    Qt: Fix timing issues on high refresh rate monitors

--- src/platform/qt/DisplayGL.h
+++ src/platform/qt/DisplayGL.h
@@ -15,12 +15,12 @@
 #endif
 #endif
 
+#include <QElapsedTimer>
 #include <QGLWidget>
 #include <QList>
 #include <QMouseEvent>
 #include <QQueue>
 #include <QThread>
-#include <QTimer>
 
 #include "platform/video-backend.h"
 
@@ -120,6 +120,7 @@ private:
 	VideoBackend* m_backend;
 	QSize m_size;
 	MessagePainter* m_messagePainter;
+	QElapsedTimer m_delayTimer;
 };
 
 }
