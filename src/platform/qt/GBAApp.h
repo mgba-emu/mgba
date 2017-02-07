be3e884ba51609b058b0da46c7e8bded3d2a7376
Vicki Pfau <vi@endrift.com>
2017-02-07 15:42:39 -0800

    Qt: Fix linking after some windows have been closed

--- src/platform/qt/GBAApp.h
+++ src/platform/qt/GBAApp.h
@@ -39,7 +39,6 @@ private:
 };
 #endif
 
-
 class GBAApp : public QApplication {
 Q_OBJECT
 
@@ -78,11 +77,11 @@ private:
 
 	Window* newWindowInternal();
 
-	void pauseAll(QList<int>* paused);
-	void continueAll(const QList<int>* paused);
+	void pauseAll(QList<Window*>* paused);
+	void continueAll(const QList<Window*>& paused);
 
 	ConfigController m_configController;
-	Window* m_windows[MAX_GBAS];
+	QList<Window*> m_windows;
 	MultiplayerController m_multiplayer;
 
 	NoIntroDB* m_db;
