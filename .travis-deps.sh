5806ae925738827cb7f1aa9dd9bd9184c2df1067
waddlesplash <waddlesplash@gmail.com>
2017-03-20 13:46:30 -0400

    Formatting

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -4,7 +4,8 @@ if [ $TRAVIS_OS_NAME = "osx" ]; then
 	brew install qt5 ffmpeg imagemagick sdl2 libzip libpng
 	if [ "$TRAVIS_OS_NAME" == "osx" -a "$CC" == "gcc" ]; then
 		brew install gcc@4.9
-		export CC=gcc-4.9; export CXX=g++-4.9;
+		export CC=gcc-4.9
+		export CXX=g++-4.9
 	fi
 else
 	sudo apt-get clean
