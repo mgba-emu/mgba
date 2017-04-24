953335846b9a6dca420875cfd5edaea055eae681
Augustin Cavalier <waddlesplash@gmail.com>
2017-04-24 00:16:11 -0400

    All: Fix build on Haiku.

--- src/util/vfs/vfs-fd.c
+++ src/util/vfs/vfs-fd.c
@@ -166,7 +166,11 @@ static bool _vfdSync(struct VFile* vf, const void* buffer, size_t size) {
 	UNUSED(size);
 	struct VFileFD* vfd = (struct VFileFD*) vf;
 #ifndef _WIN32
+#ifdef __HAIKU__
+	futimens(vfd->fd, NULL);
+#else
 	futimes(vfd->fd, NULL);
+#endif
 	if (buffer && size) {
 		return msync(buffer, size, MS_SYNC) == 0;
 	}
