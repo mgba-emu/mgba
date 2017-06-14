732ed5fa4d91c339a3591529ba9e88d03659919b
Vicki Pfau <vi@endrift.com>
2017-06-14 16:25:12 -0700

    Python: Export version info

--- src/platform/python/mgba/__init__.py
+++ src/platform/python/mgba/__init__.py
@@ -5,6 +5,8 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from ._pylib import ffi, lib
 
+from collections import namedtuple
+
 def createCallback(structName, cbName, funcName=None):
     funcName = funcName or "_py{}{}".format(structName, cbName[0].upper() + cbName[1:])
     fullStruct = "struct {}*".format(structName)
@@ -13,3 +15,19 @@ def createCallback(structName, cbName, funcName=None):
         return getattr(ffi.from_handle(h.pyobj), cbName)(*args)
 
     return ffi.def_extern(name=funcName)(cb)
+
+version = ffi.string(lib.projectVersion).decode('utf-8')
+
+GitInfo = namedtuple("GitInfo", "commit commitShort branch revision")
+
+git = {}
+if lib.gitCommit and lib.gitCommit != "(unknown)":
+    git['commit'] = ffi.string(lib.gitCommit).decode('utf-8')
+if lib.gitCommitShort and lib.gitCommitShort != "(unknown)":
+    git['commitShort'] = ffi.string(lib.gitCommitShort).decode('utf-8')
+if lib.gitBranch and lib.gitBranch != "(unknown)":
+    git['branch'] = ffi.string(lib.gitBranch).decode('utf-8')
+if lib.gitRevision > 0:
+    git['revision'] = lib.gitRevision
+
+git = GitInfo(**git)
