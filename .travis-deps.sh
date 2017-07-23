d50e9365512c831fec88b2bd689a13378a6de232
Vicki Pfau <vi@endrift.com>
2017-07-23 10:37:52 -0700

    Travis: Nuke the Linux clang build
    trusty + clang + C++14 is non-operational and Travis is so bad that they're
    only concerned with whitelisting packages piecemeal instead of providing a
    usable product, or a modern LTS like xenial

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -10,14 +10,13 @@ if [ $TRAVIS_OS_NAME = "osx" ]; then
 else
 	sudo apt-get clean
 	sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
+	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	sudo apt-get update
 	sudo apt-get install -y -q cmake libedit-dev libmagickwand-dev \
 		libpng-dev libsdl2-dev libzip-dev qtbase5-dev \
 		libqt5opengl5-dev qtmultimedia5-dev libavcodec-dev \
 		libavutil-dev libavformat-dev libavresample-dev libswscale-dev
 	if [ "$CC" == "gcc" ]; then
-		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-		sudo apt-get update
 		sudo apt-get install -y -q gcc-5 g++-5
 		export CC=gcc-5
 		export CXX=g++-5
