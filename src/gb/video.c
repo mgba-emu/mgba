546688f9fb9ea87589d786cdf7a909a34be0c904
Vicki Pfau <vi@endrift.com>
2017-08-12 14:12:49 -0700

    GB Memory: Fix HDMA count starting in mode 0 (fixes #855)

--- src/gb/video.c
+++ src/gb/video.c
@@ -304,6 +304,7 @@ void _endMode3(struct mTiming* timing, void* context, uint32_t cyclesLate) {
 	GBVideoProcessDots(video);
 	if (video->ly < GB_VIDEO_VERTICAL_PIXELS && video->p->memory.isHdma && video->p->memory.io[REG_HDMA5] != 0xFF) {
 		video->p->memory.hdmaRemaining = 0x10;
+		video->p->cpuBlocked = true;
 		mTimingDeschedule(timing, &video->p->memory.hdmaEvent);
 		mTimingSchedule(timing, &video->p->memory.hdmaEvent, 0);
 	}
