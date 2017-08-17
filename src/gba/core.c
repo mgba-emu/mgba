477f54010b2f72682365e07fcbbe6c2e1dee7e78
twinaphex <libretro@gmail.com>
2017-08-18 01:33:58 +0200

    Fix this rule

--- src/gba/core.c
+++ src/gba/core.c
@@ -239,7 +239,7 @@ static void _GBACoreLoadConfig(struct mCore* core, const struct mCoreConfig* con
 
 	mCoreConfigCopyValue(&core->config, config, "gba.bios");
 
-#if (!defined(MINIMAL_CORE) || MINIMAL_CORE < 2) && !DISABLE_THREADING
+#if ((!defined(MINIMAL_CORE) || MINIMAL_CORE < 2) && !DISABLE_THREADING)
 	mCoreConfigGetIntValue(config, "threadedVideo", &gbacore->threadedVideo);
 #endif
 }
