a8f29906140feba11bdd4cd88574aee275f6f096
Vicki Pfau <vi@endrift.com>
2017-09-24 11:56:26 -0700

    VFS: Make VFile.truncate work growing files on PSV (fixes #885)

--- src/platform/psp2/sce-vfs.c
+++ src/platform/psp2/sce-vfs.c
@@ -120,7 +120,20 @@ static void _vfsceUnmap(struct VFile* vf, void* memory, size_t size) {
 
 static void _vfsceTruncate(struct VFile* vf, size_t size) {
 	struct VFileSce* vfsce = (struct VFileSce*) vf;
-	// TODO
+	SceOff cur = sceIoLseek(vfsce->fd, 0, SEEK_CUR);
+	SceOff end = sceIoLseek(vfsce->fd, 0, SEEK_END);
+	if (end < size) {
+		uint8_t buffer[2048] = {};
+		size_t write = size - end;
+		while (write >= sizeof(buffer)) {
+			sceIoWrite(vfsce->fd, buffer, sizeof(buffer));
+			write -= sizeof(buffer);
+		}
+		if (write) {
+			sceIoWrite(vfsce->fd, buffer, write);
+		}
+	} // TODO: Else
+	sceIoLseek(vfsce->fd, cur, SEEK_SET);
 }
 
 ssize_t _vfsceSize(struct VFile* vf) {
