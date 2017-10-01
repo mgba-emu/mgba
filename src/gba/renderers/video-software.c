7f792a5d15e145d5a1a932e5ad96d20632dfa1e4
meleu <meleu@users.noreply.github.com>
2017-10-01 20:03:05 -0300

    Util: Get rid of _to16Bit

--- src/gba/renderers/video-software.c
+++ src/gba/renderers/video-software.c
@@ -592,13 +592,12 @@ static void GBAVideoSoftwareRendererDrawScanline(struct GBAVideoRenderer* render
 	}
 
 #ifdef COLOR_16_BIT
-#if defined(__ARM_NEON) && !defined(__APPLE__) && !defined(__aarch64__)
-	_to16Bit(row, softwareRenderer->row, VIDEO_HORIZONTAL_PIXELS);
-#else
-	for (x = 0; x < VIDEO_HORIZONTAL_PIXELS; ++x) {
+	for (x = 0; x < VIDEO_HORIZONTAL_PIXELS; x += 4) {
 		row[x] = softwareRenderer->row[x];
+		row[x + 1] = softwareRenderer->row[x + 1];
+		row[x + 2] = softwareRenderer->row[x + 2];
+		row[x + 3] = softwareRenderer->row[x + 3];
 	}
-#endif
 #else
 	memcpy(row, softwareRenderer->row, VIDEO_HORIZONTAL_PIXELS * sizeof(*row));
 #endif
