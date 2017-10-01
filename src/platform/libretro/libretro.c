a680fd6109fc2a2544224cf8aeea4c1a4606798e
Sérgio Benjamim <sergio_br2@yahoo.com.br>
2017-10-01 18:44:30 -0300

    Bump to 0.6.1

--- src/platform/libretro/libretro.c
+++ src/platform/libretro/libretro.c
@@ -146,7 +146,7 @@ void retro_get_system_info(struct retro_system_info* info) {
 #ifndef GIT_VERSION
 #define GIT_VERSION ""
 #endif
-	info->library_version = "0.6.0" GIT_VERSION;
+	info->library_version = "0.6.1" GIT_VERSION;
 	info->library_name = "mGBA";
 	info->block_extract = false;
 }
