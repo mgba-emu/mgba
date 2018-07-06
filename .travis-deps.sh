7876ae861b65594e7d0ba4720a7afa4fc0904fe8
Vicki Pfau <vi@endrift.com>
2018-07-05 20:50:56 -0700

    Travis: Use docker containers for applicable builds

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 if [ $TRAVIS_OS_NAME = "osx" ]; then
 	brew update
-	brew install qt5 ffmpeg imagemagick sdl2 libzip libpng
-else
-	sudo apt-get clean
-	sudo apt-get update
-	sudo apt-get install -y -q cmake libedit-dev libmagickwand-dev \
-		libpng-dev libsdl2-dev libzip-dev qtbase5-dev \
-		libqt5opengl5-dev qtmultimedia5-dev libavcodec-dev \
-		libavutil-dev libavformat-dev libavresample-dev libswscale-dev
+	brew install qt5 ffmpeg imagemagick sdl2 libedit libpng libzip
 fi
