dcf0a76519e226c42f45bfdb29f451c2a69bd012
lacastri <43101266+lacastri@users.noreply.github.com>
2018-10-01 21:33:02 -0500

    msvc: LOAD_32BE = _byteswap_ulong

--- include/mgba-util/common.h
+++ include/mgba-util/common.h
@@ -46,13 +46,14 @@ typedef intptr_t ssize_t;
 #define snprintf _snprintf
 #define strdup _strdup
 #define lseek _lseek
-#define __builtin_bswap32 _byteswap_ulong
 #define O_ACCMODE (O_RDONLY|O_WRONLY|O_RDWR)
 #elif defined(__wii__)
 #include <sys/time.h>
 typedef intptr_t ssize_t;
 #else
+#ifndef __CELLOS_LV2__
 #include <strings.h>
+#endif
 #include <unistd.h>
 #include <sys/time.h>
 #endif
@@ -179,7 +180,13 @@ typedef intptr_t ssize_t;
 #define STORE_64LE(SRC, ADDR, ARR) *(uint64_t*) ((uintptr_t) (ARR) + (size_t) (ADDR)) = SRC
 #define STORE_32LE(SRC, ADDR, ARR) *(uint32_t*) ((uintptr_t) (ARR) + (size_t) (ADDR)) = SRC
 #define STORE_16LE(SRC, ADDR, ARR) *(uint16_t*) ((uintptr_t) (ARR) + (size_t) (ADDR)) = SRC
+
+#ifdef _MSC_VER
+#define LOAD_32BE(DEST, ADDR, ARR) DEST = _byteswap_ulong(((uint32_t*) ARR)[(ADDR) >> 2])
+#else
 #define LOAD_32BE(DEST, ADDR, ARR) DEST = __builtin_bswap32(((uint32_t*) ARR)[(ADDR) >> 2])
+#endif
+
 #endif
 
 #define MAKE_MASK(START, END) (((1 << ((END) - (START))) - 1) << (START))
