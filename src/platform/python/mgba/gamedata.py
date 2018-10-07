b790dd8e7518d9ab8edfd5e18bfd1a0eecf35482
Vicki Pfau <vi@endrift.com>
2018-10-07 14:41:01 -0700

    Python: Minor API improvements

--- src/platform/python/mgba/gamedata.py
+++ src/platform/python/mgba/gamedata.py
@@ -11,10 +11,10 @@ except ImportError:
 
 def search(core):
     crc32 = None
-    if hasattr(core, 'PLATFORM_GBA') and core.platform() == core.PLATFORM_GBA:
+    if hasattr(core, 'PLATFORM_GBA') and core.platform == core.PLATFORM_GBA:
         platform = 'GBA'
         crc32 = core.crc32
-    if hasattr(core, 'PLATFORM_GB') and core.platform() == core.PLATFORM_GB:
+    if hasattr(core, 'PLATFORM_GB') and core.platform == core.PLATFORM_GB:
         platform = 'GB'
         crc32 = core.crc32
     cls = mgba_gamedata.registry.search(platform, {'crc32': crc32})
