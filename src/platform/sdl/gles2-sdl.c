6af8faa0dd51640b70bcc0bfc3aeb47f16c9ac9d
Cameron Cawley <ccawley2011@gmail.com>
2019-05-05 13:10:55 +0100

    SDL: Move Raspberry Pi initialisation code to a separate file

--- src/platform/sdl/gles2-sdl.c
+++ src/platform/sdl/gles2-sdl.c
@@ -6,6 +6,9 @@
 #include "main.h"
 
 #include "gl-common.h"
+#ifdef BUILD_RASPI
+#include "rpi-common.h"
+#endif
 
 #include <mgba/core/core.h>
 #include <mgba/core/thread.h>
@@ -26,74 +29,7 @@ void mSDLGLES2Create(struct mSDLRenderer* renderer) {
 
 bool mSDLGLES2Init(struct mSDLRenderer* renderer) {
 #ifdef BUILD_RASPI
-	bcm_host_init();
-	renderer->display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-	int major, minor;
-	if (EGL_FALSE == eglInitialize(renderer->display, &major, &minor)) {
-		printf("Failed to initialize EGL");
-		return false;
-	}
-
-	if (EGL_FALSE == eglBindAPI(EGL_OPENGL_ES_API)) {
-		printf("Failed to get GLES API");
-		return false;
-	}
-
-	const EGLint requestConfig[] = {
-		EGL_RED_SIZE, 5,
-		EGL_GREEN_SIZE, 5,
-		EGL_BLUE_SIZE, 5,
-		EGL_ALPHA_SIZE, 1,
-		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-		EGL_NONE
-	};
-
-	EGLConfig config;
-	EGLint numConfigs;
-
-	if (EGL_FALSE == eglChooseConfig(renderer->display, requestConfig, &config, 1, &numConfigs)) {
-		printf("Failed to choose EGL config\n");
-		return false;
-	}
-
-	const EGLint contextAttributes[] = {
-		EGL_CONTEXT_CLIENT_VERSION, 2,
-		EGL_NONE
-	};
-
-	int dispWidth = 240, dispHeight = 160, adjWidth;
-	renderer->context = eglCreateContext(renderer->display, config, EGL_NO_CONTEXT, contextAttributes);
-	graphics_get_display_size(0, &dispWidth, &dispHeight);
-	adjWidth = dispHeight / 2 * 3;
-
-	DISPMANX_DISPLAY_HANDLE_T display = vc_dispmanx_display_open(0);
-	DISPMANX_UPDATE_HANDLE_T update = vc_dispmanx_update_start(0);
-
-	VC_RECT_T destRect = {
-		.x = (dispWidth - adjWidth) / 2,
-		.y = 0,
-		.width = adjWidth,
-		.height = dispHeight
-	};
-
-	VC_RECT_T srcRect = {
-		.x = 0,
-		.y = 0,
-		.width = 240 << 16,
-		.height = 160 << 16
-	};
-
-	DISPMANX_ELEMENT_HANDLE_T element = vc_dispmanx_element_add(update, display, 0, &destRect, 0, &srcRect, DISPMANX_PROTECTION_NONE, 0, 0, 0);
-	vc_dispmanx_update_submit_sync(update);
-
-	renderer->window.element = element;
-	renderer->window.width = dispWidth;
-	renderer->window.height = dispHeight;
-
-	renderer->surface = eglCreateWindowSurface(renderer->display, config, &renderer->window, 0);
-	if (EGL_FALSE == eglMakeCurrent(renderer->display, renderer->surface, renderer->surface, renderer->context)) {
-		return false;
-	}
+	mRPIGLCommonInit(renderer);
 #else
 	mSDLGLCommonInit(renderer);
 #endif
@@ -112,7 +48,11 @@ bool mSDLGLES2Init(struct mSDLRenderer* renderer) {
 	renderer->gl2.d.lockAspectRatio = renderer->lockAspectRatio;
 	renderer->gl2.d.lockIntegerScaling = renderer->lockIntegerScaling;
 	renderer->gl2.d.filter = renderer->filter;
+#ifdef BUILD_RASPI
+	renderer->gl2.d.swap = mRPIGLCommonSwap;
+#else
 	renderer->gl2.d.swap = mSDLGLCommonSwap;
+#endif
 	renderer->gl2.d.init(&renderer->gl2.d, 0);
 	renderer->gl2.d.setDimensions(&renderer->gl2.d, renderer->width, renderer->height);
 
@@ -147,11 +87,7 @@ void mSDLGLES2Runloop(struct mSDLRenderer* renderer, void* user) {
 		}
 		mCoreSyncWaitFrameEnd(&context->impl->sync);
 		v->drawFrame(v);
-#ifdef BUILD_RASPI
-		eglSwapBuffers(renderer->display, renderer->surface);
-#else
 		v->swap(v);
-#endif
 	}
 }
 
@@ -160,10 +96,10 @@ void mSDLGLES2Deinit(struct mSDLRenderer* renderer) {
 		renderer->gl2.d.deinit(&renderer->gl2.d);
 	}
 #ifdef BUILD_RASPI
-	eglMakeCurrent(renderer->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-	eglDestroySurface(renderer->display, renderer->surface);
-	eglDestroyContext(renderer->display, renderer->context);
-	eglTerminate(renderer->display);
+	eglMakeCurrent(renderer->eglDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+	eglDestroySurface(renderer->eglDisplay, renderer->eglSurface);
+	eglDestroyContext(renderer->eglDisplay, renderer->eglContext);
+	eglTerminate(renderer->eglDisplay);
 	bcm_host_deinit();
 #elif SDL_VERSION_ATLEAST(2, 0, 0)
 	SDL_GL_DeleteContext(renderer->glCtx);
