67786309bc586f6e7964dfe314d08bf07365fac0
Aaron Kling <webgeek1234@gmail.com>
2019-05-26 15:19:03 -0500

    Add libretro jni build scripts
    This is to allow building the way the majority of libretro cores
    are done

---
+++ libretro-build/jni/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+CORE_DIR := $(LOCAL_PATH)/../..
+
+include $(CORE_DIR)/libretro-build/Makefile.common
+
+COREFLAGS := -DHAVE_XLOCALE -DHAVE_STRTOF_L $(RETRODEFS) $(INCLUDES)
+
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+  COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_C) $(SOURCES_CXX)
+LOCAL_CPPFLAGS  := -std=c++11 $(COREFLAGS)
+LOCAL_CFLAGS    := $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
+include $(BUILD_SHARED_LIBRARY)
