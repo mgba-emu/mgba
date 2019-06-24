06d407f130ca454bc23668051a7b73a84b9c1dd7
Vicki Pfau <vi@endrift.com>
2019-06-24 09:05:38 -0700

    GB Video: Increment BCPS/OCPS even in mode 3 (fixes #1462)

--- src/gb/io.c
+++ src/gb/io.c
@@ -499,8 +499,8 @@ void GBIOWrite(struct GB* gb, unsigned address, uint8_t value) {
 			case REG_BCPD:
 				if (gb->video.mode != 3) {
 					GBVideoProcessDots(&gb->video, 0);
-					GBVideoWritePalette(&gb->video, address, value);
 				}
+				GBVideoWritePalette(&gb->video, address, value);
 				return;
 			case REG_OCPS:
 				gb->video.ocpIndex = value & 0x3F;
@@ -510,8 +510,8 @@ void GBIOWrite(struct GB* gb, unsigned address, uint8_t value) {
 			case REG_OCPD:
 				if (gb->video.mode != 3) {
 					GBVideoProcessDots(&gb->video, 0);
-					GBVideoWritePalette(&gb->video, address, value);
 				}
+				GBVideoWritePalette(&gb->video, address, value);
 				return;
 			case REG_SVBK:
 				GBMemorySwitchWramBank(&gb->memory, value);
