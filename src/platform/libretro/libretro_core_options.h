5a8a3c2bedad73a833782e85f6e1aeaf1574e4b6
Ömercan Kömür <fpscan@gmail.com>
2019-07-31 23:45:09 +0300

    Turkish Language Support

--- src/platform/libretro/libretro_core_options.h
+++ src/platform/libretro/libretro_core_options.h
@@ -191,6 +191,135 @@ struct retro_core_option_definition option_defs_us[] = {
 
 /* RETRO_LANGUAGE_TURKISH */
 
+struct retro_core_option_definition option_defs_tr[] = {
+   {
+      "mgba_solar_sensor_level",
+      "Güneş Sensörü Seviyesi",
+      "Ortam güneş ışığının yoğunluğunu ayarlar. Boktai serisi, kartuşlarına güneş sensörü içeren oyunlar tarafından kullanılabilir.",
+      {
+         { "0",  NULL },
+         { "1",  NULL },
+         { "2",  NULL },
+         { "3",  NULL },
+         { "4",  NULL },
+         { "5",  NULL },
+         { "6",  NULL },
+         { "7",  NULL },
+         { "8",  NULL },
+         { "9",  NULL },
+         { "10", NULL },
+         { NULL, NULL },
+      },
+      "0"
+   },
+   {
+      "mgba_allow_opposing_directions",
+      "Karşı Yönlü Girdiye Çıkmaya İzin Ver",
+      "Bunu etkinleştirmek aynı anda hem sola hem de sağa (veya yukarı ve aşağı) yönlere basma / hızlı değiştirme / tutma imkanı sağlar. Bu harekete dayalı hatalara neden olabilir.",
+      {
+         { "no",  "Devre dışı bırak" },
+         { "yes", "Etkileştir" },
+         { NULL, NULL },
+      },
+      "no"
+   },
+   {
+      "mgba_gb_model",
+      "Game Boy Modeli (yeniden başlatma gerektirir)",
+      "Yüklenen içeriği belirli bir Game Boy modeliyle çalıştırır. 'Otomatik Tespit' mevcut oyun için en uygun modeli seçecektir.",
+      {
+         { "Autodetect",       "Otomatik Tespit" },
+         { "Game Boy",         NULL },
+         { "Super Game Boy",   NULL },
+         { "Game Boy Color",   NULL },
+         { "Game Boy Advance", NULL },
+         { NULL, NULL },
+      },
+      "Autodetect"
+   },
+   {
+      "mgba_use_bios",
+      "Bulunursa BIOS Dosyasını kullanın (yeniden başlatma gerektirir)",
+      "RetroArch'ın sistem dizininde varsa, öykünülmüş donanım için resmi BIOS/önyükleyici kullanır.",
+      {
+         { "ON",  "AÇIK" },
+         { "OFF", "KAPALI" },
+         { NULL, NULL },
+      },
+      "ON"
+   },
+   {
+      "mgba_skip_bios",
+      "BIOS Girişini Atla (yeniden başlatma gerektirir)",
+      "Resmi bir BIOS / önyükleyici kullanırken, başlangıç logosu animasyonunu atlayın. Bu ayar, 'Bulunursa BIOS Dosyasını Kullan' devre dışı bırakıldığında geçersiz sayılır.",
+      {
+         { "OFF", "HAYIR" },
+         { "ON",  "AÇIK" },
+         { NULL, NULL },
+      },
+      "OFF"
+   },
+   {
+      "mgba_sgb_borders",
+      "Super Game Boy Sınırlarını kullanın (yeniden başlatma gerekir)",
+      "Super Game Boy gelişmiş oyunlarını çalıştırırken Super Game Boy sınırlarını görüntüleR.",
+      {
+         { "ON",  "AÇIK" },
+         { "OFF", "KAPALI" },
+         { NULL, NULL },
+      },
+      "ON"
+   },
+   {
+      "mgba_idle_optimization",
+      "Boşta Döngü Kaldırma",
+      "'Boşta döngüler' denilen sistemi optimize ederek sistem yükünü azaltın - hiçbir şeyin olmadığı koddaki bölümler için, CPU tam hızda çalıştırır (boşa dönen bir araba gibi). Performansı arttırır ve düşük kaliteli donanımlarda etkinleştirilmesi gerekir.",
+      {
+         { "Remove Known",      "Bilinenleri Kaldır" },
+         { "Detect and Remove", "Algıla ve Kaldır" },
+         { "Don't Remove",      "Kaldırma" },
+         { NULL, NULL },
+      },
+      "Remove Known"
+   },
+   {
+      "mgba_frameskip",
+      "Kare atlama",
+      "Görsel pürüzsüzlük pahasına performansı artırmak için çerçeveleri atlayın. Burada ayarlanan değer, bir kare oluşturulduktan sonra atlanan kare sayısıdır - yani '0' = 60fps, '1' = 30fps, '2' = 15fps, vb.",
+      {
+         { "0",  NULL },
+         { "1",  NULL },
+         { "2",  NULL },
+         { "3",  NULL },
+         { "4",  NULL },
+         { "5",  NULL },
+         { "6",  NULL },
+         { "7",  NULL },
+         { "8",  NULL },
+         { "9",  NULL },
+         { "10", NULL },
+         { NULL, NULL },
+      },
+      "0"
+   },
+#if defined(COLOR_16_BIT) && defined(COLOR_5_6_5)
+   {
+      "mgba_color_correction",
+      "Renk Düzeltmesi",
+      "Çıktı renklerini gerçek GBA / GBC donanımının görüntüsüyle eşleşecek şekilde ayarlar.",
+      {
+         { "OFF",  "KAPALI" },
+         { "GBA",  "Game Boy Advance" },
+         { "GBC",  "Game Boy Color" },
+         { "Auto", "Otomatik" },
+         { NULL, NULL },
+      },
+      "OFF"
+   },
+#endif
+   { NULL, NULL, NULL, {{0}}, NULL },
+};
+
 /*
  ********************************
  * Language Mapping
@@ -216,7 +345,7 @@ struct retro_core_option_definition *option_defs_intl[RETRO_LANGUAGE_LAST] = {
    NULL,           /* RETRO_LANGUAGE_VIETNAMESE */
    NULL,           /* RETRO_LANGUAGE_ARABIC */
    NULL,           /* RETRO_LANGUAGE_GREEK */
-   NULL,           /* RETRO_LANGUAGE_TURKISH */
+   option_defs_tr, /* RETRO_LANGUAGE_TURKISH */
 };
 
 /*
