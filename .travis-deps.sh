337d3fe91a6cf9d849e2064df5a80f2a999e8422
Vicki Pfau <vi@endrift.com>
2020-02-01 12:39:39 -0800

    Travis: Fix osx deps

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $TRAVIS_OS_NAME = "osx" ]; then
 	brew update
-	brew install qt5 ffmpeg sdl2 libedit libelf libpng libzip
+	brew install qt5 ffmpeg sdl2 libelf libpng libzip
 else
 	sudo apt-get update
 	sudo apt-get -y install libseccomp2
