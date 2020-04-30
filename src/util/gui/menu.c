38e08fb97fdb14124128115761b975397aa11d7a
Issame Kastite <sinplomo@gmail.com>
2020-04-30 07:44:23 +0200

    Update menu.c
    Vita specific code for fixing the menu

--- src/util/gui/menu.c
+++ src/util/gui/menu.c
@@ -176,12 +176,21 @@ enum GUIMenuExitReason GUIShowMenu(struct GUIParams* params, struct GUIMenu* men
 			GUIFontIconMetrics(params->font, GUI_ICON_SCROLLBAR_BUTTON, &right, 0);
 			GUIFontIconMetrics(params->font, GUI_ICON_SCROLLBAR_TRACK, &w, 0);
 			right = (right - w) / 2;
+			#ifndef PSP2
+			GUIFontDrawIconSize(params->font, params->width - right - 8, top, 0, bottom - top, 0xA0FFFFFF, GUI_ICON_SCROLLBAR_TRACK);
+			GUIFontDrawIcon(params->font, params->width - 8, top, GUI_ALIGN_HCENTER | GUI_ALIGN_BOTTOM, GUI_ORIENT_VMIRROR, 0xFFFFFFFF, GUI_ICON_SCROLLBAR_BUTTON);
+			GUIFontDrawIcon(params->font, params->width - 8, bottom, GUI_ALIGN_HCENTER | GUI_ALIGN_TOP, GUI_ORIENT_0, 0xFFFFFFFF, GUI_ICON_SCROLLBAR_BUTTON);
+			
+			y = menu->index * (bottom - top) / GUIMenuItemListSize(&menu->items);
+			GUIFontDrawIcon(params->font, params->width - 8, top + y, GUI_ALIGN_HCENTER | GUI_ALIGN_TOP, GUI_ORIENT_0, 0xFFFFFFFF, GUI_ICON_SCROLLBAR_THUMB);
+			#else
 			GUIFontDrawIconSize(params->font, params->width - right, top, 0, bottom - top, 0xA0FFFFFF, GUI_ICON_SCROLLBAR_TRACK);
 			GUIFontDrawIcon(params->font, params->width, top, GUI_ALIGN_HCENTER | GUI_ALIGN_BOTTOM, GUI_ORIENT_VMIRROR, 0xFFFFFFFF, GUI_ICON_SCROLLBAR_BUTTON);
 			GUIFontDrawIcon(params->font, params->width, bottom, GUI_ALIGN_HCENTER | GUI_ALIGN_TOP, GUI_ORIENT_0, 0xFFFFFFFF, GUI_ICON_SCROLLBAR_BUTTON);
-
-			y = menu->index * (bottom - top - 16) / GUIMenuItemListSize(&menu->items);
+			
+			y = menu->index * (bottom - top) / GUIMenuItemListSize(&menu->items);
 			GUIFontDrawIcon(params->font, params->width, top + y, GUI_ALIGN_HCENTER | GUI_ALIGN_TOP, GUI_ORIENT_0, 0xFFFFFFFF, GUI_ICON_SCROLLBAR_THUMB);
+			#endif
 		}
 
 		GUIDrawBattery(params);
