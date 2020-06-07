1db01d652bedd6aaf80bcb575af05bb66b836230
Vicki Pfau <vi@endrift.com>
2020-06-06 17:24:20 -0700

    Qt: Fix unmapping zipped ROM (fixes #1777)

--- src/platform/qt/CoreManager.cpp
+++ src/platform/qt/CoreManager.cpp
@@ -65,7 +65,7 @@ CoreController* CoreManager::loadGame(const QString& path) {
 		if (vfOriginal && (size = vfOriginal->size(vfOriginal)) > 0) {
 			void* mem = vfOriginal->map(vfOriginal, size, MAP_READ);
 			vf = VFileMemChunk(mem, size);
-			vfOriginal->unmap(vfOriginal, mem, (size_t) read);
+			vfOriginal->unmap(vfOriginal, mem, size);
 			vfOriginal->close(vfOriginal);
 		}
 	}
