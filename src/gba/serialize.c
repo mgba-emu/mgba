c1794e1072b21406ae58a56496882fdf2e28770d
Vicki Pfau <vi@endrift.com>
2020-12-13 15:02:08 -0800

    Backport serialization fix from upstream

--- src/gba/serialize.c
+++ src/gba/serialize.c
@@ -152,7 +152,7 @@ bool GBADeserialize(struct GBA* gba, const struct GBASerializedState* state) {
 		LOAD_32(gba->cpu->bankedSPSRs[i], i * sizeof(gba->cpu->bankedSPSRs[0]), state->cpu.bankedSPSRs);
 	}
 	gba->cpu->privilegeMode = gba->cpu->cpsr.priv;
-	uint32_t pcMask = (gba->cpu->executionMode == MODE_THUMB ? WORD_SIZE_THUMB : WORD_SIZE_ARM) - 1;
+	uint32_t pcMask = (gba->cpu->cpsr.t ? WORD_SIZE_THUMB : WORD_SIZE_ARM) - 1;
 	if (gba->cpu->gprs[ARM_PC] & pcMask) {
 		mLOG(GBA_STATE, WARN, "Savestate has unaligned PC and is probably corrupted");
 		gba->cpu->gprs[ARM_PC] &= ~pcMask;
