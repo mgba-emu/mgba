13addc434125aae4a39ba5d806aee5e450cc0c27
Vicki Pfau <vi@endrift.com>
2020-12-19 13:18:49 -0800

    Libretro: De-copy-paste memory.c

--- src/platform/libretro/memory.c
+++ src/platform/libretro/memory.c
@@ -1,24 +1,13 @@
-/* Copyright (c) 2013-2014 Jeffrey Pfau
+/* Copyright (c) 2013-2020 Jeffrey Pfau
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include <mgba-util/memory.h>
-#include <mgba-util/vector.h>
 
 #ifdef _WIN32
-#include <windows.h>
-#elif defined(GEKKO) || defined(__PSL1GHT__) || defined(_3DS) || defined(__SWITCH__) || defined(PSP) || defined(PS2)
-/* stub */
-#elif defined(VITA)
-#include <psp2/kernel/sysmem.h>
-#include <psp2/types.h>
-DECLARE_VECTOR(SceUIDList, SceUID);
-DEFINE_VECTOR(SceUIDList, SceUID);
-
-static struct SceUIDList uids;
-static bool listInit = false;
-#else
+#include "../windows/memory.c"
+#elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
 #include <sys/mman.h>
 
 #ifndef MAP_ANONYMOUS
@@ -28,58 +17,17 @@ static bool listInit = false;
 #ifndef MAP_ANON
 #define MAP_ANON MAP_ANONYMOUS
 #endif
-#endif
+
+#include "../posix/memory.c"
+#else
 
 void* anonymousMemoryMap(size_t size) {
-#ifdef _WIN32
-	return VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-#elif defined(__PSL1GHT__) || defined(GEKKO) || defined(_3DS) || defined(__SWITCH__) || defined(PSP) || defined(PS2)
-   return (void*)malloc(size);
-#elif defined(VITA)
-	if (!listInit)
-		SceUIDListInit(&uids, 8);
-	if (size & 0xFFF)
-   {
-		// Align to 4kB pages
-		size += ((~size) & 0xFFF) + 1;
-	}
-	SceUID memblock = sceKernelAllocMemBlock("anon", SCE_KERNEL_MEMBLOCK_TYPE_USER_RW, size, 0);
-	if (memblock < 0)
-		return 0;
-	*SceUIDListAppend(&uids) = memblock;
-	void* ptr;
-	if (sceKernelGetMemBlockBase(memblock, &ptr) < 0)
-		return 0;
-	return ptr;
-#else
-	return mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-#endif
+	return calloc(1, size);
 }
 
 void mappedMemoryFree(void* memory, size_t size) {
 	UNUSED(size);
-#ifdef _WIN32
-	// size is not useful here because we're freeing the memory, not decommitting it
-	VirtualFree(memory, 0, MEM_RELEASE);
-#elif defined(__PSL1GHT__) || defined(GEKKO) || defined(_3DS) || defined(__SWITCH__) || defined(PSP) || defined(PS2)
-   free(memory);
-#elif defined(VITA)
-	UNUSED(size);
-	size_t i;
-	for (i = 0; i < SceUIDListSize(&uids); ++i)
-   {
-		SceUID uid = *SceUIDListGetPointer(&uids, i);
-		void* ptr;
-		if (sceKernelGetMemBlockBase(uid, &ptr) < 0)
-			continue;
-		if (ptr == memory)
-      {
-			sceKernelFreeMemBlock(uid);
-			SceUIDListUnshift(&uids, i, 1);
-			return;
-		}
-	}
-#else
-	munmap(memory, size);
-#endif
+	free(memory);
 }
+
+#endif
