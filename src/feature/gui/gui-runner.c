a04baac2cb7267576444c8bc320d675108cd7d17
Vicki Pfau <vi@endrift.com>
2021-03-28 18:27:07 -0700

    3DS: Fix #1294 slightly better this time

--- src/feature/gui/gui-runner.c
+++ src/feature/gui/gui-runner.c
@@ -623,14 +623,19 @@ void mGUIRun(struct mGUIRunner* runner, const char* path) {
 		int frames = 0;
 		GUIPollInput(&runner->params, 0, &keys);
 		while (keys && frames < 30) {
-			++frames;
-			runner->params.drawStart();
-			runner->drawFrame(runner, true);
-			runner->params.drawEnd();
 #ifdef _3DS
-			// XXX: Why does this fix #1294?
-			usleep(1000);
+			if (!frames) {
 #endif
+				runner->params.drawStart();
+				runner->drawFrame(runner, true);
+				runner->params.drawEnd();
+#ifdef _3DS
+			} else {
+				// XXX: Why does this fix #1294?
+				usleep(15000);
+			}
+#endif
+			++frames;
 			GUIPollInput(&runner->params, 0, &keys);
 		}
 		if (runner->unpaused) {
