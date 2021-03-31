fee8255611ddf0c4b84b37b751931aac247faf04
Vicki Pfau <vi@endrift.com>
2021-03-30 18:33:32 -0700

    GBA: Fix crash when ROM loading fails

--- src/gba/gba.c
+++ src/gba/gba.c
@@ -411,6 +411,7 @@ bool GBALoadROM(struct GBA* gba, struct VFile* vf) {
 		gba->memory.romSize = gba->pristineRomSize;
 	}
 	if (!gba->memory.rom) {
+		gba->romVf = NULL;
 		mLOG(GBA, WARN, "Couldn't map ROM");
 		return false;
 	}
