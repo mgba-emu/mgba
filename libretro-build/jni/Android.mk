851dd7e09292fc8b4a88b901236ae0c10c54b6ce
twinaphex <libretro@gmail.com>
2021-04-13 01:58:15 +0200

    (Android) There are no C++ source files so don't link against C++_static (Makefile.common) Add missing rewind.c

--- libretro-build/jni/Android.mk
+++ libretro-build/jni/Android.mk
@@ -14,7 +14,7 @@ endif
 include $(CLEAR_VARS)
 LOCAL_MODULE    := retro
 LOCAL_SRC_FILES := $(SOURCES_C) $(SOURCES_CXX)
-LOCAL_CPPFLAGS  := -std=c++11 -O3 $(COREFLAGS)
+LOCAL_CPPFLAGS  := -O3 $(COREFLAGS)
 LOCAL_CFLAGS    := -O3 $(COREFLAGS)
 LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
 LOCAL_ARM_MODE  := arm
--- libretro-build/jni/Application.mk
+++ libretro-build/jni/Application.mk
@@ -1,3 +1,2 @@
 APP_ABI := all
 APP_PLATFORM := android-16
-APP_STL := c++_static
