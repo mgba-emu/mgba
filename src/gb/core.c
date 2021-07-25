d89678a3a50883b513a07a6a61603ff553176a36
kaisermg5 <kaisermauro8@gmail.com>
2021-07-25 13:47:07 -0300

    Fixed error on GB, while attempting to deserialize extensions data.

--- src/gb/core.c
+++ src/gb/core.c
@@ -1141,6 +1141,8 @@ struct mCore* GBCoreCreate(void) {
 	core->startVideoLog = _GBCoreStartVideoLog;
 	core->endVideoLog = _GBCoreEndVideoLog;
 #endif
+	core->extDataSerialize = NULL;
+	core->extDataDeserialize = NULL;
 	return core;
 }
 
