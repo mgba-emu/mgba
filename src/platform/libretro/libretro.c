fda6556195b0f6a3cc70c3b64297ad31a064f275
Ryunam <mt.ryunam@gmail.com>
2021-10-16 03:38:10 +0200

    Update and expose emulator version string (#240)

--- src/platform/libretro/libretro.c
+++ src/platform/libretro/libretro.c
@@ -1259,7 +1259,7 @@ void retro_get_system_info(struct retro_system_info* info) {
 #ifndef GIT_VERSION
 #define GIT_VERSION ""
 #endif
-	info->library_version = GIT_VERSION;
+	info->library_version = "0.9.2" GIT_VERSION;
 	info->library_name = "mGBA";
 	info->block_extract = false;
 }
