8e51b132d5f4bfee9d1cf8cfafbbc508b91272c8
felixjones <felix.jones@mojang.com>
2022-01-06 17:54:07 +0000

    Configurable exit based on SWI with configurable register return code

--- src/platform/test/mtest-main.c
+++ src/platform/test/mtest-main.c
@@ -15,29 +15,46 @@
 
 #include <signal.h>
 
-static const char * rom_path = "C:\\Users\\Administrator\\OneDrive - Microsoft\\gba-toolchain-3\\samples\\7 Overlays\\cmake-build-debug-mingw\\helloOverlay.gba";
-
-#define MTEST_OPTIONS "F:N"
+#define MTEST_OPTIONS "S:R:N"
 #define MTEST_USAGE \
 	"\nAdditional options:\n" \
-	"  -F FRAMES        Run for the specified number of FRAMES before exiting\n" \
+	"  -S SWI       	Run until specified SWI call before exiting\n" \
+	"  -R REGISTER      Register to return as exit code\n" \
 	"  -N               Disable video rendering entirely\n" \
 
+ enum mRegisterExitCode {
+	 mRegister_NONE = -1,
+	 mRegister_GPRS = 0,
+	 mRegister_CPSR = 16
+ };
+
 struct MTestOpts {
-	unsigned long frames;
+	int exitSwiImmediate;
+	enum mRegisterExitCode returnCodeRegister;
 	bool noVideo;
 };
 
-static int _mTestRunloop(struct mCore* core, unsigned long frames);
+static int _mTestRunloop(struct mCore* core, int exitSwiImmediate, enum mRegisterExitCode registerExitCode);
 static void _mTestShutdown(int signal);
 static bool _parseMTestOpts(struct mSubParser* parser, int option, const char* arg);
+static enum mRegisterExitCode _parseNamedRegister(const char* regStr);
 
 static bool _dispatchExiting = false;
+
+#ifdef M_CORE_GBA
+void (*_armSwi16)(struct ARMCore* cpu, int immediate);
+void (*_armSwi32)(struct ARMCore* cpu, int immediate);
+
+static void _mTestSwi16(struct ARMCore* cpu, int immediate);
+static void _mTestSwi32(struct ARMCore* cpu, int immediate);
+
+static int _prevSwiImmediate = -1;
+#endif
 
 int main(int argc, char * argv[]) {
 	signal(SIGINT, _mTestShutdown);
 
-	struct MTestOpts mTestOpts = { 0 };
+	struct MTestOpts mTestOpts = { -1, mRegister_NONE, false };
 	struct mSubParser subparser = {
 		.usage = MTEST_USAGE,
 		.parse = _parseMTestOpts,
@@ -79,6 +96,11 @@ int main(int argc, char * argv[]) {
 #ifdef M_CORE_GBA
 	if (core->platform(core) == mPLATFORM_GBA) {
 		((struct GBA*) core->board)->hardCrash = false;
+
+		_armSwi16 = ((struct GBA*) core->board)->cpu->irqh.swi16;
+		((struct GBA*) core->board)->cpu->irqh.swi16 = _mTestSwi16;
+		_armSwi32 = ((struct GBA*) core->board)->cpu->irqh.swi32;
+		((struct GBA*) core->board)->cpu->irqh.swi32 = _mTestSwi32;
 	}
 #endif
 
@@ -115,7 +137,7 @@ int main(int argc, char * argv[]) {
 		savestate = 0;
 	}
 
-	_mTestRunloop(core, mTestOpts.frames);
+	int returnCode = _mTestRunloop(core, mTestOpts.exitSwiImmediate, mTestOpts.returnCodeRegister);
 
 	core->unloadROM(core);
 
@@ -131,14 +153,24 @@ loadError:
 	mCoreConfigDeinit(&core->config);
 	core->deinit(core);
 
-	return !cleanExit; // TODO : Return code from ROM
+	return cleanExit ? returnCode : 1;
 }
 
-static int _mTestRunloop(struct mCore* core, unsigned long frames) {
+static int _mTestRunloop(struct mCore* core, int exitSwiImmediate, enum mRegisterExitCode registerExitCode) {
 	do {
-		core->runFrame(core);
-		--frames;
-	} while (frames > 0 && !_dispatchExiting);
+		core->step(core);
+	} while (_prevSwiImmediate != exitSwiImmediate && !_dispatchExiting);
+
+#ifdef M_CORE_GBA
+	if (core->platform(core) == mPLATFORM_GBA) {
+		if (registerExitCode >= 0 && registerExitCode < 16) {
+			return ((struct GBA*) core->board)->cpu->regs.gprs[registerExitCode];
+		}
+		if (registerExitCode == mRegister_CPSR) {
+			return ((struct GBA*) core->board)->cpu->regs.cpsr.packed;
+		}
+	}
+#endif
 	return 0;
 }
 
@@ -147,12 +179,27 @@ static void _mTestShutdown(int signal) {
 	_dispatchExiting = true;
 }
 
+#ifdef M_CORE_GBA
+static void _mTestSwi16(struct ARMCore* cpu, int immediate) {
+	_armSwi16(cpu, immediate);
+	_prevSwiImmediate = immediate;
+}
+
+static void _mTestSwi32(struct ARMCore* cpu, int immediate) {
+	_armSwi32(cpu, immediate);
+	_prevSwiImmediate = immediate;
+}
+#endif
+
 static bool _parseMTestOpts(struct mSubParser* parser, int option, const char* arg) {
 	struct MTestOpts* opts = parser->opts;
 	errno = 0;
 	switch (option) {
-	case 'F':
-		opts->frames = strtoul(arg, 0, 10);
+	case 'S':
+		opts->exitSwiImmediate = strtol(arg, NULL, 0);
+		return !errno;
+	case 'R':
+		opts->returnCodeRegister = _parseNamedRegister(arg);
 		return !errno;
 	case 'N':
 		opts->noVideo = true;
@@ -160,4 +207,26 @@ static bool _parseMTestOpts(struct mSubParser* parser, int option, const char* a
 	default:
 		return false;
 	}
+}
+
+static enum mRegisterExitCode _parseNamedRegister(const char* regStr) {
+	if (regStr[0] == 'r' || regStr[0] == 'R') {
+		return mRegister_GPRS + strtol(regStr + 1, NULL, 10);
+	}
+
+	if (0 == strcasecmp("sp", regStr)) {
+		return mRegister_GPRS + 13;
+	}
+	if (0 == strcasecmp("lr", regStr)) {
+		return mRegister_GPRS + 14;
+	}
+	if (0 == strcasecmp("pc", regStr)) {
+		return mRegister_GPRS + 15;
+	}
+
+	if (0 == strcasecmp("cpsr", regStr)) {
+		return mRegister_CPSR;
+	}
+
+	return mRegister_NONE;
 }
