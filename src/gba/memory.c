4d01b86aaea8a66dbf4ee2ce8e8ecb85e11a7e8b
Metroid Maniac <trmetroidmaniac@gmail.com>
2022-04-16 18:55:27 +0100

    Fix 1M SRAM again

--- src/gba/memory.c
+++ src/gba/memory.c
@@ -950,6 +950,10 @@ void GBAStore16(struct ARMCore* cpu, uint32_t address, int16_t value, int* cycle
 		}
 		// Fall through
 	case REGION_CART0_EX:
+		if (address == 0x09000000 && memory->savedata.type == SAVEDATA_SRAM1M) {
+			GBASavedataSetSRAMBank(&memory->savedata, value);
+			break;
+		}
 		if (memory->hw.devices & HW_FLASHROM) {
 			if (GBAFlashROMWrite(memory, address, value)) {
 				break;
@@ -988,10 +992,6 @@ void GBAStore16(struct ARMCore* cpu, uint32_t address, int16_t value, int* cycle
 				_agbPrintStore(gba, address, value);
 				break;
 			}
-		}
-		if (address == 0x09000000 && memory->savedata.type == SAVEDATA_SRAM1M) {
-			GBASavedataSetSRAMBank(&memory->savedata, value);
-			break;
 		}
 		mLOG(GBA_MEM, GAME_ERROR, "Bad cartridge Store16: 0x%08X", address);
 		break;
