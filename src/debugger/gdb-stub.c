fe8b436b41244fd52ac001dcad319aef327d238d
Vicki Pfau <vi@endrift.com>
2023-03-06 14:57:05 -0800

    GDB: Ugh

--- src/debugger/gdb-stub.c
+++ src/debugger/gdb-stub.c
@@ -846,7 +846,7 @@ void GDBStubUpdate(struct GDBStub* stub) {
 		} else {
 			goto connectionLost;
 		}
-		SocketSetTCPPush(stub->connection);
+		SocketSetTCPPush(stub->connection, 1);
 	}
 	while (true) {
 		if (stub->shouldBlock) {
