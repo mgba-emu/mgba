ba6865b570666bf63e0fe8f8954d108cd4addf8f
Vicki Pfau <vi@endrift.com>
2023-04-26 01:05:11 -0700

    mGUI: Enable auto-softpatching (closes #2899)

--- src/feature/gui/gui-runner.c
+++ src/feature/gui/gui-runner.c
@@ -456,6 +456,7 @@ void mGUIRun(struct mGUIRunner* runner, const char* path) {
 	mLOG(GUI_RUNNER, DEBUG, "Loading save...");
 	mCoreAutoloadSave(runner->core);
 	mCoreAutoloadCheats(runner->core);
+	mCoreAutoloadPatch(runner->core);
 	if (runner->setup) {
 		mLOG(GUI_RUNNER, DEBUG, "Setting up runner...");
 		runner->setup(runner);
