ba83c4d9d35f310db40fa366d6e15ae69a7c4fc2
Vicki Pfau <vi@endrift.com>
2023-11-24 22:10:09 -0800

    SDL: Fix minor leak if debugger isn't used

--- src/platform/sdl/main.c
+++ src/platform/sdl/main.c
@@ -255,6 +255,8 @@ int mSDLRun(struct mSDLRenderer* renderer, struct mArguments* args) {
 #ifdef ENABLE_SCRIPTING
 		mScriptBridgeSetDebugger(bridge, debugger);
 #endif
+	} else {
+		mDebuggerDeinit(&debugger);
 	}
 #endif
 
