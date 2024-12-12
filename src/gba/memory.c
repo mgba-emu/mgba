655211651ad664c910b8264d6c3cb616d2ae1d75
Vicki Pfau <vi@endrift.com>
2024-12-11 16:25:17 -0800

    GBA Memory: Properly narrow down GPIO write validity check

--- src/gba/memory.c
+++ src/gba/memory.c
@@ -915,7 +915,7 @@ void GBAStore16(struct ARMCore* cpu, uint32_t address, int16_t value, int* cycle
 		break;
 	case GBA_REGION_ROM0:
 		if (IS_GPIO_REGISTER(address & 0xFFFFFE)) {
-			if (memory->hw.devices == HW_NONE) {
+			if (!(memory->hw.devices & HW_GPIO)) {
 				mLOG(GBA_HW, WARN, "Write to GPIO address %08X on cartridge without GPIO", address);
 				break;
 			}
