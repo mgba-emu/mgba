e4dc41df31cc65780bb9fff8e64cd90fea3ead12
Vicki Pfau <vi@endrift.com>
2024-12-12 03:59:21 -0800

    Qt: Rename LatAm Spanish to just "Spanish"

--- src/platform/qt/ts/mgba-es.ts
+++ src/platform/qt/ts/mgba-es.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es_419" sourcelanguage="en_US">
+<TS version="2.1" language="es" sourcelanguage="en_US">
 <context>
     <name>QGBA::AboutScreen</name>
     <message>
