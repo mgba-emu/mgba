467768d414df709c175b0de6773876930b4f1b6e
Vicki Pfau <vi@endrift.com>
2025-06-08 21:57:01 -0700

    Scripting: Expose some basic geometric utility types

--- include/mgba/script/base.h
+++ include/mgba/script/base.h
@@ -11,9 +11,12 @@
 CXX_GUARD_START
 
 #include <mgba/script/macros.h>
+#include <mgba-util/geometry.h>
 #include <mgba-util/image.h>
 
 mSCRIPT_DECLARE_STRUCT(mImage)
+mSCRIPT_DECLARE_STRUCT(mRectangle);
+mSCRIPT_DECLARE_STRUCT(mSize);
 
 struct mScriptContext;
 void mScriptContextAttachImage(struct mScriptContext* context);
