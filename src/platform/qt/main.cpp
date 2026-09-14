a1020b0e72e90d56d4779efcdf776aa9c1450fe1
Vicki Pfau <vi@endrift.com>
2026-09-13 17:30:07 -0700

    Qt: Force device pixel ratio rounding

--- src/platform/qt/main.cpp
+++ src/platform/qt/main.cpp
@@ -98,6 +98,9 @@ int main(int argc, char* argv[]) {
 #if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
 	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 #endif
+#if (QT_VERSION > QT_VERSION_CHECK(5, 14, 0))
+	QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Round);
+#endif
 
 #ifdef BUILD_GLES2
 	QSurfaceFormat format;
